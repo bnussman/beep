f3efbb3edfc7e91e910d32671475be7ce6a0a27e
Banks Nussman <banks@nussman.us>
2021-03-08 23:19:38 -0500

    monorepo setup

--- api/build/healthcheck/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Error_1 = require("../utils/Error");
-function default_1(request, response) {
-    response.send(new Error_1.APIResponse(Error_1.APIStatus.Success, "Ok"));
-}
-exports.default = default_1;
