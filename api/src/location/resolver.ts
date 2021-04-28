f21454e05af6145da8dc6da7b3347ead31037e4b
Banks Nussman <banks@nussman.us>
2021-04-27 22:57:50 -0400

    please work

--- api/src/location/resolver.ts
+++ api/src/location/resolver.ts
@@ -1,4 +1,4 @@
-import {wrap} from '@mikro-orm/core';
+import { wrap } from '@mikro-orm/core';
 import { Arg, Authorized, Ctx, Mutation, PubSub, PubSubEngine, Resolver, Root, Subscription } from 'type-graphql';
 import {BeepORM} from '../app';
 import { Location } from '../entities/Location';
@@ -11,8 +11,9 @@ export class LocationResolver {
     @Mutation(() => Boolean)
     @Authorized()
     public async insertLocation(@Ctx() ctx: Context, @Arg('location') location: LocationInput, @PubSub() pubSub: PubSubEngine): Promise<boolean> {
+        /*
         try {
-        const entry = await BeepORM.locationRepository.findOne({ user: ctx.user.id }, { refresh: true });
+        const entry = await BeepORM.locationRepository.findOne({ user: ctx.user.id }, { populate: false, refresh: true });
 
         if (!entry) {
             const e = new Location({ ...location, user: ctx.user });
@@ -33,6 +34,24 @@ export class LocationResolver {
         } catch (error) {
             console.log(error);
         }
+         */
+        try {
+            await BeepORM.userRepository.populate(ctx.user, 'location');
+
+            if (ctx.user.location) {
+                wrap(ctx.user.location).assign(location);
+            }
+            else {
+                ctx.user.location = new Location({ ...location, user: ctx.user });
+            }
+
+            pubSub.publish("Location" + ctx.user.id, ctx.user.location);
+
+            await BeepORM.userRepository.persistAndFlush(ctx.user);
+        }
+        catch (error) {
+            console.log(error);
+        }
 
         return true;
     }
@@ -44,4 +63,8 @@ export class LocationResolver {
     public getLocationUpdates(@Arg("topic") topic: string, @Root() entry: Location): Location {
         return entry;
     }
+}
+
+function warp(location: Location | undefined) {
+    throw new Error('Function not implemented.');
 }
