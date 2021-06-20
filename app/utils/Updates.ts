e4076883630b6e4d1982e8614c173fe30be780fd
Banks Nussman <banks@nussman.us>
2021-06-20 11:49:34 -0400

    Added logic to copy auth token so users tokens dont expire when we launch v2

--- app/utils/Updates.ts
+++ app/utils/Updates.ts
@@ -1,8 +1,32 @@
 import * as Updates from 'expo-updates';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Logger from './Logger';
 
 export async function handleUpdateCheck(): Promise<void> {
     if (!__DEV__) {
         const result = await Updates.checkForUpdateAsync();
-        if (result.isAvailable) Updates.reloadAsync();
+
+        if (result.isAvailable) {
+            await handleTokenMove();
+            Updates.reloadAsync();
+        }
+    }
+}
+
+async function handleTokenMove() {
+    try {
+        const data = await AsyncStorage.getItem('@user');
+
+        if (!data) return;
+
+        const userData = JSON.parse(data);
+        
+        await AsyncStorage.setItem('auth', JSON.stringify({
+            id: userData.token,
+            tokenid: userData.tokenid
+        }));
+    }
+    catch (error) {
+        Logger.error(error);
     }
 }
