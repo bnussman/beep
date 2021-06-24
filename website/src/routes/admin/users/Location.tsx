0bd343096bea9388cd444f9beb9aa95180308d5e
Banks Nussman <banks@nussman.us>
2021-06-24 19:52:39 -0400

    idk

--- website/src/routes/admin/users/Location.tsx
+++ website/src/routes/admin/users/Location.tsx
@@ -1,10 +1,11 @@
 import {gql} from '@apollo/client';
-import { Box, Center, Text } from '@chakra-ui/react';
+import { Avatar, Box, Center, Text } from '@chakra-ui/react';
 import dayjs from 'dayjs';
 import React, {useEffect} from 'react';
 import { User } from '../../../generated/graphql';
 import {client} from '../../../utils/Apollo';
 import {GetUser} from './User';
+import GoogleMapReact from 'google-map-react';
 
 interface Props {
     user: Partial<User>;
@@ -50,6 +51,8 @@ function LocationView(props: Props) {
         });
     }
 
+    const Marker = ({ text }) => <Avatar src={user.photoUrl} size="xs" />;
+
     useEffect(() => {
         subscribe();
 
@@ -69,12 +72,27 @@ function LocationView(props: Props) {
         <Box>
             <Text>{user.location?.latitude}, {user.location?.longitude}</Text>
             <Text>{dayjs().to(user.location?.timestamp)}</Text>
+            <div style={{ height: 350, width: '100%' }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBgabJrpu7-ELWiUIKJlpBz2mL6GYjwCVI' }}
+                defaultCenter={{ lat: user.location.latitude, lng: user.location.longitude }}
+                defaultZoom={15}
+            >
+                <Marker
+                    lat={user.location.latitude}
+                    lng={user.location.longitude}
+                    text={user.name}
+                />
+            </GoogleMapReact>
+            </div>
+            {/*
             <iframe
                 title="Map"
                 width="100%"
                 height="350"
                 src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBgabJrpu7-ELWiUIKJlpBz2mL6GYjwCVI&q=${user.location?.latitude},${user.location?.longitude}`}>
             </iframe>
+              */}
         </Box>
 
     );
