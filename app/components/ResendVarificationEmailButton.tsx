25d82e82a35ad098c32435c8a5d3957a419b698b
Banks Nussman <banks@nussman.us>
2021-09-15 20:23:39 -0400

    #69 maybe fix resend email

--- app/components/ResendVarificationEmailButton.tsx
+++ app/components/ResendVarificationEmailButton.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Button } from '@ui-kitten/components';
 import { EmailIcon, LoadingIndicator } from '../utils/Icons';
-import {gql, useMutation} from '@apollo/client';
-import {ResendMutation} from '../generated/graphql';
+import { gql, useMutation } from '@apollo/client';
+import { ResendMutation } from '../generated/graphql';
 
 const Resend = gql`
     mutation Resend {
@@ -10,33 +10,37 @@ const Resend = gql`
     }
 `;
 
-function ResendButton(props) {
-    const [resend, { data, loading, error }] = useMutation<ResendMutation>(Resend);
+function ResendButton(): JSX.Element {
+  const [resend, { loading }] = useMutation<ResendMutation>(Resend);
 
-    async function resendEmailVarification() {
-        const result = await resend();
-        if (result) alert("Successfuly resent varification email");
-        else alert(error);
+  async function resendEmailVarification() {
+    try {
+      await resend();
+      alert("Successfuly resent varification email");
     }
+    catch(error) {
+      alert(error.message);
+    }
+  }
 
-    if (loading) {
-        return (
-            <Button appearance='ghost' accessoryLeft={LoadingIndicator} style={{ marginBottom: 10 }}>
-                Loading
-            </Button>
-        );
-    }
-    
-    return(
-        <Button
-            onPress={() => resendEmailVarification()}
-            accessoryLeft={EmailIcon}
-            style={{ marginBottom: 10 }}
-            appearance='ghost'
-        >
-                Resend Varification Email
-        </Button>
+  if (loading) {
+    return (
+      <Button appearance='ghost' accessoryLeft={LoadingIndicator} style={{ marginBottom: 10 }}>
+        Loading
+      </Button>
     );
+  }
+
+  return (
+    <Button
+      onPress={() => resendEmailVarification()}
+      accessoryLeft={EmailIcon}
+      style={{ marginBottom: 10 }}
+      appearance='ghost'
+    >
+      Resend Varification Email
+    </Button>
+  );
 }
 
 export default ResendButton;
