ec83717543b14ac88cd2bd8e92ad0ed4a98684c3
Banks Nussman <banks@nussman.us>
2022-01-22 13:16:38 -0500

    remove metro config

--- app/metro.config.js
+++
@@ -1,6 +0,0 @@
-const { getDefaultConfig } = require("metro-config");
-const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues();
-exports.resolver = {
-  ...defaultResolver,
-  sourceExts: [...defaultResolver.sourceExts, "cjs"],
-};
