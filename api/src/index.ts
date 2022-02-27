0083c7f12b246f507872f4db3370f89b50981a86
Banks Nussman <banks@nussman.us>
2022-02-26 19:51:01 -0500

    attempt to fix build

--- api/src/index.ts
+++ api/src/index.ts
@@ -61,7 +61,7 @@ async function getContext(data: ExpressContext, orm: MikroORM<IDatabaseDriver<Co
 }
 
 async function onSubscribe(
-  { connectionParams }: Context<Extra & Partial<Record<PropertyKey, never>>>,
+  { connectionParams }: Context<Extra>,
   msg: SubscribeMessage,
   schema: GraphQLSchema,
   orm: MikroORM<IDatabaseDriver<Connection>>
