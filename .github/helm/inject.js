eb8d2d258d3453ab30c32b75bffb36d13fd192be
Banks Nussman <banks@nussman.us>
2022-05-01 19:54:47 -0400

    debug and check

--- .github/helm/inject.js
+++ .github/helm/inject.js
@@ -5,8 +5,12 @@ let content = '';
 const keys = Object.keys(process.env);
 
 for (const key of keys) {
-  content += `\n  ${key}: ${process.env[key]}`
+  if (process.env[key]) {
+    content += `\n  ${key}: ${process.env[key]}`
+  }
 }
+
+console.log(content);
 
 fs.appendFile(`${__dirname}/templates/configmap.yaml`, content, err => {
   if (err) {
