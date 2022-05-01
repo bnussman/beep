a1e61bb291ef5c058310385fbafb114902b6fc61
Banks Nussman <banks@nussman.us>
2022-05-01 18:13:36 -0400

    try to fix node

--- .github/helm/inject.js
+++ .github/helm/inject.js
@@ -3,7 +3,7 @@ let context = '';
 const keys = Object.keys(process.env);
 
 for (const key of keys) {
-  output += `  ${key}: ${process.env[key]}\n`
+  context += `  ${key}: ${process.env[key]}\n`
 }
 
 fs.appendFile('.gitlab/helm/templates/configmap.yaml', content, err => {
