5db6daba8b91f900f8dba45cc4a129d7e27be5a3
Banks Nussman <banks@nussman.us>
2022-05-01 18:10:26 -0400

    run

--- .github/helm/inject.js
+++ .github/helm/inject.js
@@ -1,1 +1,14 @@
-console.log("HERE IS THE ENV", process.env);
+let context = '';
+
+const keys = Object.keys(process.env);
+
+for (const key of keys) {
+  output += `  ${key}: ${process.env[key]}\n`
+}
+
+fs.appendFile('.gitlab/helm/templates/configmap.yaml', content, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+});
