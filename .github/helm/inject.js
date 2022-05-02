bd721c51bc1ed35be8f167e6bd6e04e3db26a23a
Banks Nussman <banks@nussman.us>
2022-05-01 22:16:05 -0400

    exclude some secrets

--- .github/helm/inject.js
+++ .github/helm/inject.js
@@ -4,11 +4,23 @@ let content = '';
 
 console.log(process.env.secrets);
 
+const excludedSecrets = [
+  "github_token",
+  "DOCKERHUB_TOKEN",
+  "DOCKERHUB_USERNAME",
+  "KUBE_CONFIG",
+];
+
 const secrets = JSON.parse(process.env.secrets);
 
 const keys = Object.keys(secrets);
 
 for (const key of keys) {
+
+  if (excludedSecrets.includes(key)) {
+    continue;
+  }
+
   content += `\n  ${key}: "${String(secrets[key])}"`
 }
 
