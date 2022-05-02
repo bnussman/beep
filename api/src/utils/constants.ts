4220a2fe98a397cdcea9e7601eb34da21e1ea9a8
Banks Nussman <banks@nussman.us>
2022-05-02 18:18:47 -0400

    update var names

--- api/src/utils/constants.ts
+++ api/src/utils/constants.ts
@@ -2,13 +2,13 @@ export const isProduction = process.env.NODE_ENV === "production";
 
 export const ENVRIONMENT: 'staging' | 'production' | 'development' = process.env.ENVIRONMENT_NAME as 'staging' | 'production' ?? "development";
 
-export const DB_URL = process.env.POSTGRESQL_URL || "postgresql://localhost:5432";
+export const DB_URL = process.env.DB_URL || "postgresql://localhost:5432";
 
-export const DB_DATABASE = process.env.POSTGRESQL_DATABASE || "beep";
+export const DB_DATABASE = process.env.DB_DATABASE || "beep";
 
-export const DB_PASSWORD = process.env.POSTGRESQL_PASSWORD || "beep";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "beep";
 
-export const DB_USER = process.env.POSTGRESQL_USER || "beep";
+export const DB_USER = process.env.DB_USER || "beep";
 
 export const REDIS_HOST = process.env.REDIS_HOST || "localhost";
 
