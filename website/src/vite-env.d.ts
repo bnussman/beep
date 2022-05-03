83e6c85f377a1c643eb3813764d70dda3654343c
Banks Nussman <banks@nussman.us>
2022-05-02 21:31:38 -0400

    new urls

--- website/src/vite-env.d.ts
+++ website/src/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  VITE_GOOGLE_API_KEY: string
+  VITE_GOOGLE_API_KEY: string;
+  ENVIRONMENT_NAME: 'preview' | 'production' | undefined;
 }
