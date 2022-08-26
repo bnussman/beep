e7fe5ce8ed21a9aff1209d4b6c7bc8756daab230
Banks Nussman <banks@nussman.us>
2022-08-26 16:11:37 -0400

    🔧 remove old migrations

--- api/migrations/Migration20210820031139.ts
+++
@@ -1,9 +0,0 @@
-import { Migration } from '@mikro-orm/migrations';
-
-export class Migration20210820031139 extends Migration {
-
-  async up(): Promise<void> {
-    this.addSql('alter table "queue_entry" add constraint "queue_entry_rider_id_unique" unique ("rider_id");');
-  }
-
-}
