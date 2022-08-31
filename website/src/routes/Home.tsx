a6043d9682fe13aaabcbcbff1614eaf22ab97e5e
Banks Nussman <banks@nussman.us>
2022-08-31 12:24:36 -0400

    📱 new mockups for home page

--- website/src/routes/Home.tsx
+++ website/src/routes/Home.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import iPhone from '../assets/iphone.png';
-import { Container } from "@chakra-ui/react";
+import iPhoneDark from '../assets/dark-square.png';
+import iPhoneLight from '../assets/light-square.png';
+import { Container, useColorMode } from "@chakra-ui/react";
 import { Hero } from "../components/Hero";
 
 export function Home() {
+  const { colorMode } = useColorMode();
+
   return (
     <Container maxW="container.xl">
       <Hero
         title="Ride Beep App"
         subtitle="Your ultimate ride-share app. A simple way to get a ride around college campuses. A product by students for students."
-        image={iPhone}
+        image={colorMode === 'dark' ? iPhoneLight : iPhoneDark}
         buttonLink="/download"
         buttonText="Download"
       />
