4692c2f591f75a40ac63807f8a71091893ced876
Banks Nussman <banks@nussman.us>
2022-10-05 14:21:37 -0400

    add car entity

---
+++ api/src/entities/Car.ts
@@ -0,0 +1,35 @@
+import { PrimaryKey, Authorized, Field, ObjectType } from "type-graphql";
+import { Entity, Property } from "@mikro-orm/core";
+import { v4 } from "uuid";
+
+@ObjectType()
+@Entity()
+export class Car {
+  @PrimaryKey()
+  @Field()
+  id: string = v4();
+
+  @Field()
+  @Property()
+  make!: string;
+
+  @Field()
+  @Property()
+  model!: string;
+
+  @Field()
+  @Property()
+  photo!: string;
+
+  @Field()
+  @Property()
+  year!: number;
+
+  @Field()
+  @Property()
+  created: Date = new Date();
+
+  @Field()
+  @Property({ onUpdate: () => new Date() })
+  updated: Date = new Date();
+}
