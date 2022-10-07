64111383a75c8d709c57f0bb282424b7d779c1d8
Banks Nussman <banks@nussman.us>
2022-10-07 17:36:27 -0400

    try to fix web app

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,6 +1,7 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 
 module.exports = async function (env, argv) {
+  env.mode = 'development';
   const config = await createExpoWebpackConfigAsync(
     {
       ...env,
