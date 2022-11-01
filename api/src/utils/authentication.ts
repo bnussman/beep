d57c06058578ce34abb70050da4447f72fdb6ebd
Banks Nussman <banks@nussman.us>
2022-11-01 13:05:19 -0400

    🧽 auth clean up

--- api/src/utils/authentication.ts
+++ api/src/utils/authentication.ts
@@ -1,4 +1,4 @@
-import { UserRole } from "../entities/User";
+import { User, UserRole } from "../entities/User";
 import { AuthChecker, MiddlewareFn } from "type-graphql";
 import { Context } from "../utils/context";
 import { AuthenticationError } from "apollo-server-core";
@@ -34,32 +34,38 @@ export const authChecker: AuthChecker<Context> = ({ args, context }, roles) => {
 export const LeakChecker: MiddlewareFn<Context> = async ({ context, info }, next) => {
   const result = await next();
 
+  // If user is not authed, just trust that auth was already handled.
   if (!context?.user) {
     return result;
   }
 
+  // Let admins see anything
   if (context.user.role === UserRole.ADMIN) {
     return result;
   }
 
-  //@ts-expect-error ill fix later
-  if ((["email", "phone", "location"].includes(info.fieldName) && context.user[info.fieldName] !== result) || (info.fieldName === "cars" && result[0]?.user.id !== context?.user?.id)) {
-    // a protectd value is trying to used
-    const entry = await context.em.findOne(QueueEntry, { state: { $gt: 0 }, $or: [ { rider: context.user.id }, { beeper: context.user.id} ] });
-
-    const hasAcceptedEntry = entry !== null && entry !== undefined;
-
-    if (hasAcceptedEntry) {
-      return result;
-    } else {
-      console.log(`returning null because queue is not accepted | trying to get ${info.fieldName} | ${context.user.name()}`)
-
-      if (info.fieldName === "cars") {
-        return [];
-      }
-
-      return null;
+  // If a user is trying to get cars and the car is not their own, make sure they are in a beep with the other user.
+  if (info.fieldName === "cars" && result[0]?.user.id !== context?.user?.id) {
+    if (!result[0]?.user.id) {
+      throw new Error("Unable to get user id from cars array when checking auth");
     }
+    const beep = await context.em.findOne(QueueEntry, { state: { $gt: 0 }, rider: context.user.id, beeper: result[0]?.user.id });
+
+    if (beep) {
+      return result;
+    }
+    
+    return [];
+  }
+
+  if ((["email", "phone", "location"].includes(info.fieldName) && context.user[info.fieldName as keyof User] !== result)) {
+    const beep = await context.em.findOne(QueueEntry, { state: { $gt: 0 }, $or: [ { rider: context.user.id }, { beeper: context.user.id} ] });
+
+    if (beep) {
+      return result;
+    } 
+
+    return null;
   }
 
   return result;
