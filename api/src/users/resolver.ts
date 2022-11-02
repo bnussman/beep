0f798747307c6337428a8cdf0f79169fe7b377d1
Banks Nussman <banks@nussman.us>
2022-11-02 14:22:03 -0400

    🔒 allow query by location because we have the leak checker

--- api/src/users/resolver.ts
+++ api/src/users/resolver.ts
@@ -34,7 +34,7 @@ export class UserResolver {
   @Query(() => User)
   @Authorized('No Verification')
   public async getUser(@Ctx() ctx: Context, @Info() info: GraphQLResolveInfo, @Arg("id", { nullable: true }) id?: string): Promise<User> {
-    const populate = fieldsToRelations(info, { excludeFields: ['location'] }) as Array<keyof User>;
+    const populate = fieldsToRelations(info) as Array<keyof User>;
 
     return await ctx.em.findOneOrFail(User, id || ctx.user.id, { populate });
   }
