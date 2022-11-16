651b579d5fc53dc5ac1494d7f71b04d0c003bec7
Banks Nussman <banks@nussman.us>
2022-11-16 14:13:53 -0500

    🎨 use system color mode

--- website/src/utils/theme.ts
+++ website/src/utils/theme.ts
@@ -2,6 +2,10 @@ import { extendTheme, StyleFunctionProps } from "@chakra-ui/react";
 import { mode } from '@chakra-ui/theme-tools';
 
 export const theme = extendTheme({
+  config: {
+    initialColorMode: 'system',
+    useSystemColorMode: true,
+  },
   fonts: {
     heading: "poppins",
     body: "poppins",
