d569725cd50f767928b916b1c23b7787aa6ac470
Banks Nussman <banks@nussman.us>
2023-01-03 20:09:30 -0500

    ✨ better `website` dark mode

--- website/src/utils/theme.ts
+++ website/src/utils/theme.ts
@@ -29,5 +29,36 @@ export const theme = extendTheme({
         },
       },
     },
+    Card: {
+      baseStyle: {
+        header: {
+          pb: 0,
+        },
+        footer: {
+          pt: 0,
+          justifyContent: 'flex-end',
+        },
+      }
+    },
+    Modal: {
+      baseStyle: (props: StyleFunctionProps) => ({
+        dialog: {
+          bg: mode(props.theme.semanticTokens.colors['chakra-body-bg']._light, 'rgb(14, 17, 19)')(props),
+        }
+      })
+    },
+    Menu: {
+      baseStyle: (props: StyleFunctionProps) => ({
+        list: {
+          bg: mode(props.theme.semanticTokens.colors['chakra-body-bg']._light, 'rgb(14, 17, 19)')(props),
+        },
+        item: {
+          bg: mode(props.theme.semanticTokens.colors['chakra-body-bg']._light, 'rgb(14, 17, 19)')(props),
+          _hover: {
+            bg: mode('rgb(245, 245, 245)', 'rgb(20, 24, 28)')(props),
+          },
+        },
+      })
+    },
   },
 });
