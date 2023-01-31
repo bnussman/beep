d282126c7f94070d81113cffa5bab9bd529b1098
Banks Nussman <banks@nussman.us>
2023-01-31 11:07:35 -0500

    delete `QueueEntry` and change `Beep`

--- api/src/entities/Beep.ts
+++ api/src/entities/Beep.ts
@@ -1,54 +1,65 @@
-import { Entity, Filter, ManyToOne, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, Enum, Filter, ManyToOne, PrimaryKey, Property } from "@mikro-orm/core";
 import { Field, ObjectType } from "type-graphql";
 import { User } from "./User";
 import { v4 } from 'uuid';
-import { QueueEntry } from "./QueueEntry";
+
+export enum Status {
+  DENIED = "denied",
+  WAITING = "waiting",
+  ACCEPTED = "accepted",
+  ON_THE_WAY = "on_the_way",
+  HERE = "here",
+  IN_PROGRESS = "in_progress",
+  COMPLETE = "complete",
+}
 
 @ObjectType()
 @Entity()
 @Filter({ name: 'in', cond: args => ({ $or: [{ beeper: args.id } , { rider: args.id }] })})
 export class Beep {
-    
-    @PrimaryKey()
-    @Field()
-    id: string = v4();
-
-    @Field(() => User)
-    @ManyToOne(() => User)
-    beeper!: User;
-
-    @Field(() => User)
-    @ManyToOne(() => User)
-    rider!: User;
-    
-    @Field()
-    @Property()
-    origin!: string;
-
-    @Field()
-    @Property()
-    destination!: string;
-
-    @Field()
-    @Property()
-    groupSize!: number;
-
-    @Field()
-    @Property() 
-    start!: Date;
-
-    @Field()
-    @Property() 
-    end!: Date;
-
-    constructor(entry: QueueEntry) {
-        this.id = entry.id;
-        this.beeper = entry.beeper;
-        this.rider = entry.rider;
-        this.origin = entry.origin;
-        this.destination = entry.destination;
-        this.groupSize = entry.groupSize;
-        this.start = new Date(entry.start * 1000);
-        this.end = new Date();
+  constructor(values?: Partial<Beep>) {
+    if (values) {
+      Object.assign(this, values);
     }
+  }
+
+  @PrimaryKey()
+  @Field()
+  id: string = v4();
+
+  @Field(() => User)
+  @ManyToOne(() => User)
+  beeper!: User;
+
+  @Field(() => User)
+  @ManyToOne(() => User)
+  rider!: User;
+
+  @Field()
+  @Property()
+  origin!: string;
+
+  @Field()
+  @Property()
+  destination!: string;
+
+  @Field()
+  @Property()
+  groupSize!: number;
+
+  @Field()
+  @Property()
+  start!: Date;
+
+  @Field()
+  @Property()
+  end!: Date;
+
+  @Field()
+  @Enum(() => Status)
+  status!: Status;
+
+  @Field()
+  @Property({ persist: false })
+  position: number = -1;
 }
