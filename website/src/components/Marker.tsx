28c22d7376d82a7742216952a04bd8003ee65b4b
Banks Nussman <banks@nussman.us>
2023-04-05 22:48:35 -0400

    use Apple Maps 🗺️

--- website/src/components/Marker.tsx
+++ website/src/components/Marker.tsx
@@ -1,98 +1,18 @@
 import React from "react";
-import { Marker as _Marker } from 'react-map-gl';
-import { Link } from "react-router-dom";
-import { QueuePreview } from "./QueuePreview";
-import {
-  Avatar,
-  Box,
-  Text,
-  Center,
-  useClipboard,
-  useToast,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverFooter,
-  PopoverHeader,
-  PopoverTrigger,
-  Portal,
-  HStack,
-  Stack,
-  Tooltip,
-} from "@chakra-ui/react";
+import { Marker as _Marker } from 'mapkit-react';
 
 interface Props {
   latitude: number;
   longitude: number;
   username: string;
-  userId: string;
-  photo: string | null | undefined;
   name: string;
-  variant?: 'queue' | 'default';
 }
 
 export function Marker(props: Props) {
-  const { latitude, longitude, variant, userId, username, photo, name } = props;
-  const { onCopy } = useClipboard(`${latitude},${longitude}`);
-  const toast = useToast();
+  const { latitude, longitude, name, username } = props;
 
-  const copy = () => {
-    onCopy();
-    toast({ title: "Copied coordinates to clipboard", status: "info" });
-  };
-
-  if (variant === 'queue') {
-    return (
-      <_Marker longitude={longitude} latitude={latitude}>
-        <Popover isLazy>
-          <PopoverTrigger>
-            <Box width="max-content" cursor="pointer">
-              <Center>
-                <Avatar src={photo || ''} size="xs" />
-              </Center>
-              <Text>{name}</Text>
-            </Box>
-          </PopoverTrigger>
-          <Portal>
-            <PopoverContent>
-              <PopoverArrow />
-              <PopoverHeader>
-                <Link to={`/admin/users/${userId}/queue`}>
-                  <HStack>
-                    <Avatar src={photo || ''} />
-                    <Stack spacing={0}>
-                      <Text fontWeight="extrabold">{name}</Text>
-                      <Text>@{username}</Text>
-                    </Stack>
-                  </HStack>
-                </Link>
-              </PopoverHeader>
-              <PopoverCloseButton />
-              <PopoverBody>
-                <QueuePreview userId={userId} />
-              </PopoverBody>
-              <PopoverFooter cursor="pointer" onClick={copy}>{latitude.toFixed(3)} {longitude.toFixed(3)}</PopoverFooter>
-            </PopoverContent>
-          </Portal>
-        </Popover>
-      </_Marker>
-    );
-  }
 
   return (
-    <_Marker latitude={latitude} longitude={longitude}>
-      <Box width="max-content" onClick={copy}>
-        <Tooltip label={`${latitude}, ${longitude}`} aria-label={`${latitude}, ${longitude}`}>
-          <Box>
-            <Center>
-              <Avatar src={photo || ''} size="xs" />
-            </Center>
-            <Text>{name}</Text>
-          </Box>
-        </Tooltip>
-      </Box>
-    </_Marker>
+    <_Marker title={name} subtitle={username} longitude={longitude} latitude={latitude} />
   );
 };
--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
 import App from './App'
-import 'mapbox-gl/dist/mapbox-gl.css';
 
 // Refer to https://reactjs.org/blog/2022/03/29/react-v18.html#new-strict-mode-behaviors 
 // and https://github.com/mui/material-ui/issues/21250#issuecomment-1165363989
