44b5c67f35abcf6a809db5f7734021fb858955bd
Banks Nussman <banks@nussman.us>
2023-04-09 15:23:45 -0400

    🛠 add venmo dep

--- api/src/admin/resolver.ts
+++ api/src/admin/resolver.ts
@@ -1,7 +1,8 @@
 import Redis from "ioredis";
 import { UserRole } from "../entities/User";
 import { REDIS_HOST, REDIS_PASSWROD } from "../utils/constants";
-import { Authorized, Query, Resolver } from "type-graphql";
+import { Authorized, Mutation, Query, Resolver } from "type-graphql";
+import { Venmo } from "@banksnussman/venmo";
 
 @Resolver()
 export class AdminResolver {
@@ -17,5 +18,23 @@ export class AdminResolver {
     const channels = await redis.pubsub("CHANNELS") as string[];
 
     return channels;
+  }
+
+  @Mutation(() => Number)
+  @Authorized(UserRole.ADMIN)
+  public async makePayment(): Promise<number> {
+    const venmo = new Venmo({
+      username: process.env.VENMO_USERNAME ?? "",
+      password: process.env.VENMO_PASSWORD ?? "",
+      bankAccountNumber: process.env.VENMO_BANK_ACCOUNT_NUMBER ?? "",
+    });
+
+    const balance = await venmo.pay({
+      username: "Ian-Murphy-35",
+      amount: 0.02,
+      note: "OMG!"
+    })
+
+    return balance;
   }
 }
