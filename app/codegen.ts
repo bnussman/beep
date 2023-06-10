9f1095829b8600499178bc2bc97317f06b7ceb24
Banks Nussman <banks@nussman.us>
2023-06-10 11:20:01 -0400

    ✈️ migrate to ts codegen configs

---
+++ app/codegen.ts
@@ -0,0 +1,26 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:3001/graphql",
+  documents: ["./**/*.tsx", "./utils/*.ts"],
+  generates: {
+    "generated/graphql.tsx": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+      config: {
+        withHooks: true,
+        withComponent: false,
+        withHOC: false,
+        apolloReactHooksImportFrom: "@apollo/client",
+        apolloReactCommonImportFrom: "@apollo/client",
+        gqlImport: "@apollo/client#gql",
+      },
+    },
+  },
+};
+
+export default config;
