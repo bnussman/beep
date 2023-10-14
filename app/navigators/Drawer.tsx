3bc1ed9a2812253cf9435012052a72d03fec4f55
Banks Nussman <banks@nussman.us>
2023-10-14 18:53:22 -0400

    change: use pumpkin

--- app/navigators/Drawer.tsx
+++ app/navigators/Drawer.tsx
@@ -139,7 +139,7 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
               </Text>
             </Stack>
             <Spacer />
-            <Text fontSize="3xl" px={2}>👻</Text>
+            <Text fontSize="3xl" px={2}>🎃</Text>
           </HStack>
         </Pressable>
         <VStack divider={<Divider />} space={4}>
