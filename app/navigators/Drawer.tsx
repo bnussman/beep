6976f8b6f3c24ede6f08348121fa665bae948d74
Banks Nussman <banks@nussman.us>
2023-10-14 16:48:22 -0400

    change: show ghost

--- app/navigators/Drawer.tsx
+++ app/navigators/Drawer.tsx
@@ -138,7 +138,8 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
               </Text>
             </Stack>
             <Spacer />
-            <Text fontSize="3xl" px={2}>🎃</Text>
+            // <Text fontSize="3xl" px={2}>🎃</Text>
+            <Text fontSize="3xl" px={2}>👻</Text>
           </HStack>
         </Pressable>
         <VStack divider={<Divider />} space={4}>
