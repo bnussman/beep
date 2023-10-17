415ddd29d50567c85893cacf22e07d23554d1ad1
Banks Nussman <banks@nussman.us>
2023-10-16 20:29:22 -0400

    try to fix CI

--- api/src/directions/resolver.ts
+++ api/src/directions/resolver.ts
@@ -20,6 +20,7 @@ export class DirectionsResolver {
   @Query(() => String)
   public async getETA(@Arg('start') start: string, @Arg('end') end: string): Promise<string> {
     return "ETAs will be back soon!";
+    /*
     const result = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${start}&destination=${end}&key=${getRandom(keys)}`);
 
     const data = await result.json();
@@ -32,11 +33,13 @@ export class DirectionsResolver {
     }
 
     return eta;
+    */
   }
 
   @Query(() => [Suggestion])
   public async getLocationSuggestions(@Arg('location') location: string, @Arg('sessiontoken') sessiontoken: string): Promise<Suggestion[]> {
     return [];
+    /*
     const result = await fetch(encodeURI(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${location}&key=${getRandom(keys)}&sessiontoken=${sessiontoken}`));
 
     const data = await result.json();
@@ -48,5 +51,6 @@ export class DirectionsResolver {
     }
 
     return output;
+    */
   }
 }
