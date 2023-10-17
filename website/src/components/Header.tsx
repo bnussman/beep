88dd2698c296f1d02a36f08aa26a6c2c8c7a908d
Banks Nussman <banks@nussman.us>
2023-10-16 23:12:30 -0400

    chore: better html maybe

--- website/src/components/Header.tsx
+++ website/src/components/Header.tsx
@@ -22,7 +22,7 @@ export function Header() {
   const user = data?.getUser;
 
   return (
-    <Flex h={16} alignItems='center' justifyContent='space-between' px={4} mb={4} borderBottom="1px" bg={useColorModeValue("rgba(255, 255, 255, 0.8)", "rgb(20, 24, 28, 0.85)")} borderBottomColor={useColorModeValue("gray.100", "#32373e")} position="fixed" w="full" zIndex={999} css={{ backdropFilter: 'blur(4px)' }}>
+    <Flex as="nav" h={16} alignItems='center' justifyContent='space-between' px={4} mb={4} borderBottom="1px" bg={useColorModeValue("rgba(255, 255, 255, 0.8)", "rgb(20, 24, 28, 0.85)")} borderBottomColor={useColorModeValue("gray.100", "#32373e")} position="fixed" w="full" zIndex={999} css={{ backdropFilter: 'blur(4px)' }}>
       <HStack spacing={4} alignItems='center'>
         <Heading
           as={Link}
