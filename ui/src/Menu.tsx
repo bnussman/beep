505cdc99cad82277cdc073f35d1be166401d9ec3
Banks Nussman <banks@nussman.us>
2024-02-26 20:15:59 -0500

    refactor: menu improvments

--- ui/src/Menu.tsx
+++ ui/src/Menu.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ListItem, ListItemProps, Popover, Separator, YGroup } from "tamagui";
+import { ListItem, ListItemProps, Popover, Separator, Stack, YGroup } from "tamagui";
 
 interface Props {
   items: ListItemProps[];
@@ -10,7 +10,7 @@ export function Menu({ items, Trigger }: Props) {
   const [open, setOpen] = useState(false);
 
   return (
-    <Popover size="$5" allowFlip open={open} onOpenChange={setOpen}>
+    <Popover open={open} onOpenChange={setOpen} placement="bottom">
       <Popover.Trigger asChild>
         {Trigger}
       </Popover.Trigger>
@@ -32,23 +32,24 @@ export function Menu({ items, Trigger }: Props) {
         ]}
       >
         <Popover.Arrow borderWidth={1} borderColor="$borderColor" />
-
-        <YGroup alignSelf="center" bordered width={240} size="$5" separator={<Separator />}>
-          {items.map((item, index) => (
-            <YGroup.Item key={index}>
-              <ListItem
-                hoverTheme
-                pressTheme
-                title="Unknown"
-                {...item}
-                onPress={(e) => {
-                  item.onPress?.(e);
-                  setOpen(false);
-                }}
-              />
-            </YGroup.Item>
-          ))}
-        </YGroup>
+        <Stack>
+          <YGroup width={240} separator={<Separator />}>
+            {items.map((item, index) => (
+              <YGroup.Item key={index}>
+                <ListItem
+                  hoverTheme
+                  pressTheme
+                  title="Unknown"
+                  {...item}
+                  onPress={(e) => {
+                    item.onPress?.(e);
+                    setOpen(false);
+                  }}
+                />
+              </YGroup.Item>
+            ))}
+          </YGroup>
+        </Stack>
       </Popover.Content>
     </Popover>
   );
