669f9215c3dd5dc22c3a15f6b8d0dcbb9438c95a
Banks Nussman <banks@nussman.us>
2024-03-19 20:45:16 -0400

    feat: add users with duplicate queries query for project

--- api/src/admin/resolver.ts
+++ api/src/admin/resolver.ts
@@ -3,7 +3,6 @@ import { User, UserRole } from "../entities/User";
 import { REDIS_HOST, REDIS_PASSWROD } from "../utils/constants";
 import { Authorized, Ctx, Query, Resolver } from "type-graphql";
 import { Context } from "../utils/context";
-import { sql } from "@mikro-orm/core";
 
 @Resolver()
 export class AdminResolver {
@@ -21,24 +20,16 @@ export class AdminResolver {
     return channels;
   }
 
-  @Query(() => [String])
+  @Query(() => [User])
   @Authorized(UserRole.ADMIN)
-  public async getUsersWithDuplicateEmails(@Ctx() ctx: Context): Promise<string[]> {
-    // SELECT DISTINCT lower(email) FROM public.user
-    //  WHERE UPPER(email) INy
-    // (SELECT UPPER(email) FROM public.user GROUP BY UPPER(email) HAVING COUNT(*) > 1)
-    //
-    // { ['COUNT(email)']: { $gt: 1 }}
+  public async getUsersWithDuplicateEmails(@Ctx() ctx: Context): Promise<User[]> {
+    const usersWithDuplicateEmails = await ctx.em.execute(`
+     SELECT * FROM "user"
+     WHERE UPPER(email) IN
+     (SELECT UPPER(email) FROM "user" GROUP BY UPPER(email) HAVING COUNT(*) > 1)
+     ORDER BY email
+   `);
 
-    const qb2 = ctx.em.createQueryBuilder(User).select(sql`lower(email)`).groupBy(sql`lower(email)`).having("COUNT(*) > 1");
-
-    console.log("---", qb2.getKnexQuery())
-    const qb1 = ctx.em.createQueryBuilder(User).select(sql`lower(email)`, true).where({ [sql`lower(email)`]: { $in: qb2.getKnexQuery() }});
-
-    const result = await qb1.execute();
-
-    console.log(result);
-
-    return [];
+    return usersWithDuplicateEmails as User[];
   }
 }
