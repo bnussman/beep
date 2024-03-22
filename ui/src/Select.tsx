55da25d45de8136658fb5caf1690a9e046a1da55
Banks Nussman <banks@nussman.us>
2024-03-22 00:13:59 -0400

    chore: clean up tamagui select component

--- ui/src/Select.tsx
+++ ui/src/Select.tsx
@@ -1,7 +1,6 @@
-import { Check, ChevronDown, ChevronUp } from '@tamagui/lucide-icons';
-import { LinearGradient } from 'tamagui/linear-gradient';
 import React from 'react';
-import { Select as _Select, Adapt, SelectProps, Sheet, YStack } from 'tamagui';
+import { Check, ChevronDown } from '@tamagui/lucide-icons';
+import { Select as _Select, Adapt, SelectProps, Sheet } from 'tamagui';
 
 interface Props<T extends { label: string, value: string }> extends SelectProps {
   items: T[];
@@ -9,7 +8,6 @@ interface Props<T extends { label: string, value: string }> extends SelectProps 
 }
 
 export function Select<T extends { label: string, value: string }>({ items, placeholder, ...props }: Props<T>) {
-  const native = true;
   return (
     <_Select native {...props}>
       <_Select.Trigger width="100%" iconAfter={ChevronDown}>
@@ -18,7 +16,6 @@ export function Select<T extends { label: string, value: string }>({ items, plac
 
       <Adapt platform="touch">
         <Sheet
-          native={!!native}
           modal
           dismissOnSnapToBottom
           animationConfig={{
@@ -28,7 +25,7 @@ export function Select<T extends { label: string, value: string }>({ items, plac
             stiffness: 250,
           }}
         >
-          <Sheet.Handle />
+          <Sheet.Handle backgroundColor="$gray8" />
           <Sheet.Frame>
             <Sheet.ScrollView>
               <Adapt.Contents />
@@ -42,89 +39,23 @@ export function Select<T extends { label: string, value: string }>({ items, plac
         </Sheet>
       </Adapt>
 
-      <_Select.Content zIndex={200000}>
-        <_Select.ScrollUpButton
-          alignItems="center"
-          justifyContent="center"
-          position="relative"
-          width="100%"
-          height="$3"
-        >
-          <YStack zIndex={10}>
-            <ChevronUp size={20} />
-          </YStack>
-          <LinearGradient
-            start={[0, 0]}
-            end={[0, 1]}
-            fullscreen
-            colors={['$background', 'transparent']}
-            borderRadius="$4"
-          />
-        </_Select.ScrollUpButton>
-
-        <_Select.Viewport
-          // to do animations:
-          // animation="quick"
-          // animateOnly={['transform', 'opacity']}
-          // enterStyle={{ o: 0, y: -10 }}
-          // exitStyle={{ o: 0, y: 10 }}
-          minWidth={200}
-        >
+      <_Select.Content>
+        <_Select.Viewport minWidth={200}>
           <_Select.Group>
-            {/* <_Select.Label>Fruits</_Select.Label> */}
-            {/* for longer lists memoizing these is useful */}
-            {items.map((item, i) => {
-              return (
-                <_Select.Item
-                  index={i}
-                  key={item.label}
-                  value={item.value}
-                >
-                  <_Select.ItemText>{item.label}</_Select.ItemText>
-                  <_Select.ItemIndicator marginLeft="auto">
-                    <Check size={16} />
-                  </_Select.ItemIndicator>
-                </_Select.Item>
-              )
-            })}
+            {items.map((item, i) => (
+              <_Select.Item
+                index={i}
+                key={item.label}
+                value={item.value}
+              >
+                <_Select.ItemText>{item.label}</_Select.ItemText>
+                <_Select.ItemIndicator marginLeft="auto">
+                  <Check size={16} />
+                </_Select.ItemIndicator>
+              </_Select.Item>
+            ))}
           </_Select.Group>
-          {/* Native gets an extra icon */}
-          {native && (
-            <YStack
-              position="absolute"
-              right={0}
-              top={0}
-              bottom={0}
-              alignItems="center"
-              justifyContent="center"
-              width={'$4'}
-              pointerEvents="none"
-            >
-              <ChevronDown
-                size={16}
-              />
-            </YStack>
-          )}
         </_Select.Viewport>
-
-        <_Select.ScrollDownButton
-          alignItems="center"
-          justifyContent="center"
-          position="relative"
-          width="100%"
-          height="$3"
-        >
-          <YStack zIndex={10}>
-            <ChevronDown size={20} />
-          </YStack>
-          <LinearGradient
-            start={[0, 0]}
-            end={[0, 1]}
-            fullscreen
-            colors={['transparent', '$background']}
-            borderRadius="$4"
-          />
-        </_Select.ScrollDownButton>
       </_Select.Content>
     </_Select>
   );
