6e948059f5420e3f72fa76ba8adfb9af13b3f5b3
Banks Nussman <banks@nussman.us>
2024-06-19 00:26:55 -0400

    enable debug logs

--- api/src/mikro-orm.config.ts
+++ api/src/mikro-orm.config.ts
@@ -1,25 +1,34 @@
 import { LoadStrategy } from "@mikro-orm/core";
-import { defineConfig } from '@mikro-orm/postgresql';
-import { Migrator } from '@mikro-orm/migrations';
-import { DB_CA, DB_DATABASE, DB_PASSWORD, DB_URL, DB_USER, isDevelopment } from "./utils/constants";
+import { defineConfig } from "@mikro-orm/postgresql";
+import { Migrator } from "@mikro-orm/migrations";
+import {
+  DB_CA,
+  DB_DATABASE,
+  DB_PASSWORD,
+  DB_URL,
+  DB_USER,
+  isDevelopment,
+} from "./utils/constants";
 
 export default defineConfig({
-  entities: ['./build/entities/*.js'],
-  entitiesTs: ['./src/entities/*.ts'],
+  entities: ["./build/entities/*.js"],
+  entitiesTs: ["./src/entities/*.ts"],
   user: DB_USER,
   password: DB_PASSWORD,
   clientUrl: `${DB_URL}/${DB_DATABASE}`,
   loadStrategy: LoadStrategy.JOINED,
-  debug: isDevelopment,
-  driverOptions: DB_CA ? {
-    connection: {
-      ssl: {
-        ca: DB_CA,
+  debug: true,
+  driverOptions: DB_CA
+    ? {
+        connection: {
+          ssl: {
+            ca: DB_CA,
+          },
+        },
       }
-    }
-  } : {},
+    : {},
   extensions: [Migrator],
   migrations: {
     disableForeignKeys: false,
-  }
+  },
 });
