789e85ac6515828c89611745ff3ffa83b8ae87fb
Banks Nussman <banks@nussman.us>
2024-07-14 00:10:22 -0400

    clean uo

--- deploy/index.ts
+++ deploy/index.ts
@@ -3,7 +3,9 @@ import * as linode from "@pulumi/linode";
 import * as k8s from "@pulumi/kubernetes";
 import * as selfSignedCert from "@pulumi/tls-self-signed-cert";
 
-const lkeCluster = new linode.LkeCluster("cluster", {
+const lkeCluster = new linode.LkeCluster(
+  "cluster",
+  {
     region: "us-east",
     k8sVersion: "1.30",
     label: "cluster",
@@ -13,7 +15,8 @@ const lkeCluster = new linode.LkeCluster("cluster", {
         count: 3,
       }
     ],
-});
+  }
+);
 
 const namespaceName = "beep";
 const appName = "api";
@@ -22,18 +25,28 @@ const k8sProvider = new k8s.Provider("k8sProvider", {
     kubeconfig: lkeCluster.kubeconfig.apply(x => Buffer.from(x, 'base64').toString()),
 });
 
-const namespace = new k8s.core.v1.Namespace(namespaceName, { metadata: { name: namespaceName, labels: { name: namespaceName }}}, { provider: k8sProvider });
-
-const deployment = new k8s.apps.v1.Deployment(appName, {
-  metadata: {
-    name: appName,
-    namespace: namespace.metadata.name,
-    labels: { app: appName }
+const namespace = new k8s.core.v1.Namespace(namespaceName, {
+  metadata:
+    {
+      name: namespaceName,
+      labels: { name: namespaceName }
+    }
   },
-  spec: {
-    selector: { matchLabels: { app: appName } },
-    replicas: 3,
-    template: {
+  { provider: k8sProvider }
+);
+
+const deployment = new k8s.apps.v1.Deployment(
+  appName,
+  {
+    metadata: {
+      name: appName,
+      namespace: namespace.metadata.name,
+      labels: { app: appName }
+    },
+    spec: {
+      selector: { matchLabels: { app: appName } },
+      replicas: 3,
+      template: {
         metadata: { labels: { app: appName } },
         spec: {
           containers: [
@@ -50,38 +63,50 @@ const deployment = new k8s.apps.v1.Deployment(appName, {
             }
           ]
         }
+      }
     }
-  }
-}, { provider: k8sProvider });
-
-const config = new k8s.core.v1.ConfigMap(appName, {
-  metadata: {
-    name: appName,
-    namespace: namespaceName,
   },
-  data: process.env as Record<string, string>,
-}, { provider: k8sProvider });
+  { provider: k8sProvider }
+);
+
+const config = new k8s.core.v1.ConfigMap(
+  appName,
+  {
+    metadata: {
+      name: appName,
+      namespace: namespaceName,
+    },
+    data: process.env as Record<string, string>,
+  },
+  { provider: k8sProvider }
+);
 
 const cert = new selfSignedCert.SelfSignedCertificate("cert", {
-    dnsName: "api.dev.ridebeep.app",
-    validityPeriodHours: 807660,
-    localValidityPeriodHours: 17520,
-    subject: {
-        commonName: "beep-api-cert",
-        organization: "Ride Beep App",
-    },
+  dnsName: "api.dev.ridebeep.app",
+  validityPeriodHours: 807660,
+  localValidityPeriodHours: 17520,
+  subject: {
+    commonName: "beep-api-cert",
+    organization: "Ride Beep App",
+  },
 });
 
-const secret = new k8s.core.v1.Secret(appName, {
-  metadata: { name: "cert", namespace: namespace.metadata.name },
-  type: "kubernetes.io/tls",
-  stringData: {
-    ['tls.crt']: cert.pem,
-    ['tls.key']: cert.privateKey
+const secret = new k8s.core.v1.Secret(
+  appName,
+  {
+    metadata: { name: "cert", namespace: namespace.metadata.name },
+    type: "kubernetes.io/tls",
+    stringData: {
+      ['tls.crt']: cert.pem,
+      ['tls.key']: cert.privateKey
+    },
   },
-}, { provider: k8sProvider });
+  { provider: k8sProvider }
+);
 
-const service = new k8s.core.v1.Service(appName, {
+const service = new k8s.core.v1.Service(
+  appName,
+  {
     metadata: {
       name: appName,
       namespace: namespaceName,
@@ -92,11 +117,13 @@ const service = new k8s.core.v1.Service(appName, {
       }
     },
     spec: {
-        type: "LoadBalancer",
-        ports: [{ port: 443, targetPort: 3000 }],
-        selector: { app: appName }
+      type: "LoadBalancer",
+      ports: [{ port: 443, targetPort: 3000 }],
+      selector: { app: appName }
     }
-}, { provider: k8sProvider });
+  },
+  { provider: k8sProvider }
+);
 
 export const clusterLabel = lkeCluster.label;
 
