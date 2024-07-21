39fc82954dc0fc5ff423c22e9bf27963d28b9cf5
Banks Nussman <banks@nussman.us>
2024-07-20 20:57:21 -0400

    fix docker dir attempt 3

--- deploy/index.ts
+++ deploy/index.ts
@@ -18,7 +18,7 @@ const imageName = `ghcr.io/bnussman/beep:${envName === 'staging' ? 'main' : envN
 const imageResource = new docker.Image("imageResource", {
   imageName: imageName,
   build: {
-    dockerfile: "../",
+    dockerfile: "../Dockerfile",
   },
   registry: {
     password: env.GITHUB_TOKEN,
