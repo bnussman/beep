6df66f324d7a79c66d95f3e937b5017458e2ebbb
Banks Nussman <banks@nussman.us>
2024-08-06 19:42:24 -0400

    more work in apinext

---
+++ apinext/src/db.ts
@@ -0,0 +1,15 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { DB_CA, DB_DATABASE, DB_PASSWORD, DB_URL, DB_USER } from "./constants";
+import * as  schema from '../drizzle/schema';
+
+const queryClient = postgres(DB_URL, {
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
+  ssl: {
+   ca: DB_CA
+  }
+});
+
+export const db = drizzle(queryClient, { schema });
