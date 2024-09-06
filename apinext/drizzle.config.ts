ca8ab10017ade1f75b4dcf68ee71d8b831d25ddd
Banks Nussman <banks@nussman.us>
2024-09-05 23:36:14 -0400

    use drizzle to init the db in e2e tests

--- apinext/drizzle.config.ts
+++ apinext/drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "drizzle-kit";
-import { DB_CA, DB_URL } from "./src/utils/constants";
+import { DB_CA, DB_DATABASE, DB_URL } from "./src/utils/constants";
 
 export default defineConfig({
 	schema: 'drizzle/schema.ts',
 	dialect: "postgresql",
+	extensionsFilters: ["postgis"],
 	dbCredentials: {
 	  url: DB_URL,
+		database: DB_DATABASE,
 		ssl: {
  		  ca: DB_CA,
 		},
