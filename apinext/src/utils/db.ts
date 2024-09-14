ca1977521ea5c03c0e7ff6a61a60f78f80fb9dec
Banks Nussman <banks@nussman.us>
2024-09-13 22:34:13 -0400

    use neon for staging part 2

--- apinext/src/utils/db.ts
+++ apinext/src/utils/db.ts
@@ -1,14 +1,22 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { DB_CA, DB_URL } from "./constants";
+import { DB_CA, DB_HOST, DB_URL } from "./constants";
 import * as  schema from '../../drizzle/schema';
 
-const queryClient = postgres(DB_URL,
-  DB_CA ? {
-    ssl: {
-      ca: DB_CA
-    }
-  } : undefined,
-);
+function getOptions() {
+  if (DB_HOST.includes('neon')) {
+    return { ssl: 'require' as const };
+  }
+
+  if (DB_CA) {
+    return {
+      ssl: { ca: DB_CA }
+    };
+  }
+
+  return undefined
+}
+
+const queryClient = postgres(DB_URL, getOptions());
 
 export const db = drizzle(queryClient, { schema, logger: true });
