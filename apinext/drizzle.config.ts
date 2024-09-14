b8e0a291362a66d28ee47b9685f107742f9dce3f
Banks Nussman <banks@nussman.us>
2024-09-13 22:41:00 -0400

    fix e2e test

--- apinext/drizzle.config.ts
+++ apinext/drizzle.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "drizzle-kit";
-import { DB_CA, DB_DATABASE, DB_HOST, DB_PASSWORD, DB_URL, DB_USER } from "./src/utils/constants";
+import { DB_CA, DB_DATABASE, DB_HOST, DB_URL } from "./src/utils/constants";
 
 export default defineConfig({
 	schema: 'drizzle/schema.ts',
 	dialect: "postgresql",
 	extensionsFilters: ["postgis"],
 	dbCredentials: {
-	  host: DB_HOST,
-		user: DB_USER,
-		password: DB_PASSWORD,
+	  url: DB_URL,
 		database: DB_DATABASE,
     ssl: DB_HOST.includes('neon') ? "require" : { ca: DB_CA },
 	},
