709cc20071719489c411ea51ebfcc9dd8b9071de
Banks Nussman <banks@nussman.us>
2024-09-13 22:26:45 -0400

    use neon for staging

--- apinext/drizzle.config.ts
+++ apinext/drizzle.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "drizzle-kit";
-import { DB_CA, DB_DATABASE, DB_URL } from "./src/utils/constants";
+import { DB_CA, DB_DATABASE, DB_HOST, DB_PASSWORD, DB_URL, DB_USER } from "./src/utils/constants";
 
 export default defineConfig({
 	schema: 'drizzle/schema.ts',
 	dialect: "postgresql",
 	extensionsFilters: ["postgis"],
 	dbCredentials: {
-	  url: DB_URL,
+	  host: DB_HOST,
+		user: DB_USER,
+		password: DB_PASSWORD,
 		database: DB_DATABASE,
-		ssl: {
- 		  ca: DB_CA,
-		},
+    ssl: DB_HOST.includes('neon') ? "require" : { ca: DB_CA },
 	},
 	verbose: true,
 	strict: true,
