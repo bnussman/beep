32149b52a0c01ee02fd60c49f0145c8d97349f75
dylanmason <dylan.mason01@gmail.com>
2024-09-20 16:45:24 -0400

    Fix lowercase consistency

--- app/routes/cars/utils.ts
+++ app/routes/cars/utils.ts
@@ -13,10 +13,6 @@ export const colors = [
   "Silver",
 ];
 
-export function capitalize(value: string) {
-  return value.charAt(0).toUpperCase() + value.slice(1);
-}
-
 export const years = Array.from({ length: 50 }, (_, i) =>
   String(new Date().getFullYear() + 1 - i)
 );
