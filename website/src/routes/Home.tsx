e8ee1e37e796d2ee3770c8c16d422ce141fd82aa
Banks Nussman <banks@nussman.us>
2024-09-28 17:42:42 -0400

    chore: clean up website home page

--- website/src/routes/Home.tsx
+++ website/src/routes/Home.tsx
@@ -2,9 +2,23 @@ import React from 'react';
 import iPhoneDark from '../assets/dark.png';
 import iPhoneLight from '../assets/light.png';
 import { Container, useColorMode } from "@chakra-ui/react";
-import { Hero } from "../components/Hero";
 import { createRoute } from '@tanstack/react-router';
 import { rootRoute } from '../utils/router';
+import { DownloadIcon } from '@chakra-ui/icons';
+import { Link } from '@tanstack/react-router';
+import { trpc } from '../utils/trpc';
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Heading,
+  Stack,
+  StatGroup,
+  Stat,
+  StatLabel,
+  StatNumber,
+} from "@chakra-ui/react"
 
 export const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
@@ -14,16 +28,69 @@ export const indexRoute = createRoute({
 
 export function Home() {
   const { colorMode } = useColorMode();
+  const { data: userCount } = trpc.user.userCount.useQuery();
+  const { data: beepsCount } = trpc.beep.beepsCount.useQuery();
 
   return (
     <Container maxW="container.xl">
-      <Hero
-        title="Ride Beep App"
-        subtitle="A rideshare app for students. Ride or drive at your college or university today."
-        image={colorMode === 'light' ? iPhoneLight : iPhoneDark}
-        buttonLink="/download"
-        buttonText="Download"
-      />
+      <Flex
+        align="center"
+        justify={{ base: "center", md: "space-around", xl: "space-between" }}
+        direction={{ base: "column-reverse", md: "row" }}
+        minH="70vh"
+        px={8}
+        mb={16}
+      >
+        <Stack
+          spacing={4}
+          w={{ base: "80%", md: "40%" }}
+          align={["center", "center", "flex-start", "flex-start"]}
+        >
+          <Heading
+            as="h1"
+            size="xl"
+            fontWeight="bold"
+            color="primary.800"
+            textAlign={["center", "center", "left", "left"]}
+          >
+            Ride Beep App
+          </Heading>
+          <Heading
+            as="h2"
+            size="md"
+            color="primary.800"
+            opacity="0.8"
+            fontWeight="normal"
+            lineHeight={1.5}
+            textAlign={["center", "center", "left", "left"]}
+          >
+            A rideshare app for students. Ride or drive at your college or university today.
+          </Heading>
+          <Button
+            as={Link}
+            to="/download"
+            target="_blank"
+            size="lg"
+            rightIcon={<DownloadIcon />}
+            colorScheme="yellow"
+          >
+            Download
+          </Button>
+          <StatGroup gap={12} mt={8}>
+            <Stat>
+              <StatLabel>Users</StatLabel>
+              <StatNumber>{userCount ?? 0}</StatNumber>
+            </Stat>
+            <Stat>
+              <StatLabel>Rides</StatLabel>
+              <StatNumber>{beepsCount ?? 0}</StatNumber>
+            </Stat>
+          </StatGroup>
+        </Stack>
+        <Box w={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }}>
+          <Image maxH="800px" src={colorMode === 'light' ? iPhoneLight : iPhoneDark} />
+        </Box>
+      </Flex>
     </Container>
   );
 }
