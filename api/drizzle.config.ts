2dc443b65ebb22e1a281f09067e72b5690ad94fa
Banks Nussman <banks@nussman.us>
2024-10-01 23:58:17 -0400

    fix: dizzle tooling

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "drizzle-kit";
 import { DB_DATABASE, DB_HOST, DB_URL, ENVIRONMENT } from "./src/utils/constants";
 
+const ssl = DB_HOST.includes('neon') || ENVIRONMENT === "production" ? "?sslmode=require" : '';
+
 export default defineConfig({
 	schema: 'drizzle/schema.ts',
 	dialect: "postgresql",
 	extensionsFilters: ["postgis"],
 	dbCredentials: {
-	  url: DB_URL,
-		database: DB_DATABASE,
-    ssl: DB_HOST.includes('neon') || ENVIRONMENT === "production" ? "require" : undefined,
+	  url: DB_URL + ssl,
 	},
 	verbose: true,
 	strict: true,
