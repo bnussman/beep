7f5fe8ee7969e3b9b5acede75ec97b3cd38eafdd
Banks Nussman <banks@nussman.us>
2024-11-12 20:43:27 -0500

    chore: improve healthcheck endpoint

--- api/src/routers/health.ts
+++ api/src/routers/health.ts
@@ -5,18 +5,45 @@ import { sql } from "drizzle-orm";
 
 export const healthRouter = router({
   /**
-   *  Access this at http://localhost:3000/health.healthcheck
+   *  Access this at http://localhost:3000/health.healthcheck or (https://api.ridebeep.app/health.healthcheck)
    */
   healthcheck: publicProcedure
     .query(async () => {
-      const redisPing = await redisClient.ping(); // Should throw is redis is offline
-
-      const isRedisHealthy = redisPing === "PONG";
-
-      const databasePing = await dbClient.execute(sql`select 1 = 1`); // Show throw if the Database is offline
-
-      const isDatabaseHealthy = Object.values(databasePing[0])[0] === true;
-
-      return { redis: isRedisHealthy, db: isDatabaseHealthy };
+      return {
+        uptime: process.uptime(),
+        services: {
+          redis: await getRedisStatus(),
+          db: await getDatabaseStatus()
+        }
+      };
     })
 });
+
+async function getRedisStatus() {
+  const start = performance.now();
+  const redisPing = await redisClient.ping(); // Should throw is redis is offline
+
+  const end = performance.now();
+
+  const isRedisHealthy = redisPing === "PONG";
+
+  return {
+     latency: end - start,
+     online: isRedisHealthy,
+   };
+}
+
+async function getDatabaseStatus() {
+  const start = performance.now();
+
+  const databasePing = await dbClient.execute(sql`select 1 = 1`); // Show throw if the Database is offline
+
+  const end = performance.now();
+
+  const isDatabaseHealthy = Object.values(databasePing[0])[0] === true;
+
+  return {
+    latency: end - start,
+    online: isDatabaseHealthy,
+  };
+}
