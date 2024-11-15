2afaaa9c54d6222370a795ce46b98fba2d14d5e9
Banks Nussman <banks@nussman.us>
2024-11-14 23:15:01 -0500

    omg, sentry integration works when using a pool

--- api/src/utils/db.ts
+++ api/src/utils/db.ts
@@ -1,11 +1,13 @@
 import * as  schema from '../../drizzle/schema';
-import { Client } from "pg";
+import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { DB_HOST, DB_URL, ENVIRONMENT } from "./constants";
 
 const ssl = DB_HOST.includes('neon') || ENVIRONMENT === "production" ? "?sslmode=require" : '';
 
-const queryClient = new Client(DB_URL);
+const queryClient = new Pool({
+  connectionString: DB_URL + ssl
+});
 
 await queryClient.connect();
 
