a46bd53add4a6532ddce838fcc98faf31a442f51
Banks Nussman <banks@nussman.us>
2024-11-18 23:30:44 -0500

    qRevert "fix: ssl"
    This reverts commit 064d58e9446a8c935a7b1a622b1d1caa8250931d.

--- api/src/utils/db.ts
+++ api/src/utils/db.ts
@@ -6,10 +6,7 @@ import { DB_HOST, DB_URL, ENVIRONMENT } from "./constants";
 const ssl = DB_HOST.includes('neon') || ENVIRONMENT === "production" ? "?sslmode=require" : '';
 
 const queryClient = new Pool({
-  connectionString: DB_URL + ssl,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: DB_URL + ssl
 });
 
 await queryClient.connect();
