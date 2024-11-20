1aebf9792fd7079b7bb813793a99e0e8cdf78cd9
Banks Nussman <banks@nussman.us>
2024-11-19 20:17:09 -0500

    clean up

--- api/src/index.ts
+++ api/src/index.ts
@@ -49,33 +49,6 @@ const CORS_HEADERS = {
   'Access-Control-Allow-Headers': 'Content-Type, Authorization, Vary',
 };
 
-// const handler = createHTTPHandler({
-//   middleware: cors(),
-//   router: appRouter,
-//   createContext,
-// });
-
-// const httpServer = createServer((req, res) => {
-//   const request = incomingMessageToRequest(req, { maxBodySize: 20_000 });
-
-//   if (request.url.endsWith("/payments/webhook")) {
-//     handlePaymentWebook(request, res);
-//   } else {
-//     handler(req, res);
-//   }
-// });
-
-// const wss = new ws.Server({ server: httpServer });
-
-// applyWSSHandler<AppRouter>({
-//   onError: console.error,
-//   wss,
-//   router: appRouter,
-//   createContext,
-// });
-
-// httpServer.listen(3000);
-
 const bunHandler = createBunHttpHandler({
   router: appRouter,
   endpoint: '/',
