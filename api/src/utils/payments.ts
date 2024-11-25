42a3fab778a3c20a28fe6103a03b0f6b1e16ed47
Banks Nussman <banks@nussman.us>
2024-11-24 19:28:04 -0500

    readd v1 function

--- api/src/utils/payments.ts
+++ api/src/utils/payments.ts
@@ -3,7 +3,7 @@ import { REVENUE_CAT_SECRET, REVENUE_CAT_WEBHOOK_TOKEN } from "./constants";
 import { db } from "./db";
 import { user, productEnum, payment, storeEnum } from "../../drizzle/schema";
 import * as Sentry from '@sentry/bun';
-import { Webhook } from "./revenuecat";
+import { SubscriberResponse, Webhook } from "./revenuecat";
 import { paths } from "./revenucat-api";
 
 type Product = typeof productEnum.enumValues[number];
@@ -21,13 +21,7 @@ export const productExpireTimes: Record<Product, number> = {
   top_of_beeper_list_3_hours: (3 * 60 * 60 * 1000),
 }
 
-const API_ROOT = 'https://api.revenuecat.com';
-
-/**
- * Makes a fetch to https://api.revenuecat.com/v1/subscribers/:id
- * to sync our database with RevenuCat's purchases
- */
-export async function syncUserPayments(userId: string) {
+export async function syncUserPaymentsV2(userId: string) {
   if (!userId) {
     throw new Error("No user id provided when syncing payments.");
   }
@@ -58,7 +52,7 @@ export async function syncUserPayments(userId: string) {
   }
 
   do {
-    const url = `${API_ROOT}/v2/projects/d7da55a3/customers/${userId}/purchases?${queryParams.toString()}`
+    const url = `https://api.revenuecat.com/v2/projects/d7da55a3/customers/${userId}/purchases?${queryParams.toString()}`
     const request = await fetch(url, options);
 
     const response: paths['/projects/{project_id}/customers/{customer_id}/purchases']['get']['responses']['200']['content']['application/json'] = await request.json();
@@ -107,6 +101,61 @@ export async function syncUserPayments(userId: string) {
   return activePayments;
 }
 
+export async function syncUserPaymentsV1(userId: string) {
+  if (!userId) {
+    throw new Error("No user id provided when syncing payments.");
+  }
+
+  if (!REVENUE_CAT_SECRET) {
+    throw new Error("No REVENU_CAT_SECRET in env.");
+  }
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${REVENUE_CAT_SECRET}`
+    }
+  };
+
+  const request = await fetch(`https://api.revenuecat.com/v1/subscribers/${userId}`, options);
+
+  const response: SubscriberResponse = await request.json();
+
+  const products = Object.keys(response.subscriber.non_subscriptions) as Product[];
+
+  for (const product of products) {
+    for (const paymentItem of response.subscriber.non_subscriptions[product]) {
+
+      const created = new Date(paymentItem.purchase_date);
+
+      try {
+        await db.insert(payment).values({
+          id: paymentItem.id,
+          user_id: userId,
+          store: paymentItem.store as Store,
+          storeId: paymentItem.store_transaction_id,
+          price: String(productPrice[product]),
+          productId: product,
+          created,
+          expires: new Date(created.getTime() + productExpireTimes[product])
+        })
+      } catch (error) {
+        console.error("payment might already be stored", error)
+      }
+    }
+  }
+
+  const activePayments = await db.query.payment.findMany({
+    where: and(
+      eq(payment.user_id, userId),
+      gte(payment.expires, new Date())
+    ),
+  });
+
+  return activePayments;
+}
+
 /**
  * A webhook RevenueCat uses to let us know a payment was made.
  */
@@ -118,7 +167,7 @@ export async function handlePaymentWebook(request: Request) {
   }
 
   try {
-    await syncUserPayments(data.event.app_user_id);
+    await syncUserPaymentsV1(data.event.app_user_id);
   } catch (error) {
     Sentry.captureException(error);
     return new Response("Internal Server Error", { status: 500 });
