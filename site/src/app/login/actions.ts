bbbe9d10531241f4aff3eb5ac0d264562cefec66
Banks Nussman <banks@nussman.us>
2024-11-30 11:45:44 -0500

    more

--- site/src/app/login/actions.ts
+++ site/src/app/login/actions.ts
@@ -3,22 +3,22 @@ import { trpc } from '@/utils/trpc'
 import { TRPCClientError } from '@trpc/client';
 import { cookies } from 'next/headers';
 import { redirect, RedirectType } from 'next/navigation'
- 
+
 export async function login(state: { message: string }, formData: FormData) {
     try {
-    const result = await trpc.auth.login.mutate({
-        username: formData.get('username') as string ?? '',
-        password: formData.get('password') as string ?? ''
-    });
-    
-    if (result) {
-        const cookieStore = await cookies();
-        cookieStore.set('token', result.tokens.id);
-    }
-    } catch(error)  {
+        const result = await trpc.auth.login.mutate({
+            username: formData.get('username') as string ?? '',
+            password: formData.get('password') as string ?? ''
+        });
+
+        if (result) {
+            const cookieStore = await cookies();
+            cookieStore.set('token', result.tokens.id);
+        }
+    } catch (error) {
         return { message: (error as TRPCClientError<any>).message }
     }
-    
+
     redirect('/', RedirectType.replace)
 }
 
--- site/src/app/login/page.tsx
+++ site/src/app/login/page.tsx
@@ -2,7 +2,7 @@
 import Form from 'next/form'
 import { login } from './actions';
 import { useActionState } from 'react';
-import { Button, Card, Stack, TextField } from '@mui/material';
+import { Alert, Button, Card, Stack, TextField } from '@mui/material';
 
 export default function Login() {
   const [error, formAction] = useActionState(login, { message: '' });
@@ -11,7 +11,7 @@ export default function Login() {
     <Card sx={{ p: 2 }}>
       <Form action={formAction}>
         <Stack spacing={2}>
-          {error?.message && <p>{error.message}</p>}
+          {error?.message && <Alert severity="error">{error.message}</Alert>}
           <TextField label="Username or Email" name="username" />
           <TextField label="Password" name="password" type="password" />
           <Button variant="contained" type="submit">Login</Button>
