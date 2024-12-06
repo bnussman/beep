81a14a30578f52845b9aecbe256decec3c6df3ae
Banks Nussman <banks@nussman.us>
2024-12-05 19:07:27 -0500

    fix: typescript error

--- app/routes/beep/PremiumBanner.tsx
+++ app/routes/beep/PremiumBanner.tsx
@@ -1,8 +1,8 @@
+import React from 'react';
 import { Button } from "@/components/Button";
 import { Card } from "@/components/Card";
 import { Countdown } from "@/components/CountDown";
 import { Text } from "@/components/Text";
-import { trpc } from "@/utils/trpc";
 import { useNavigation } from "@react-navigation/native";
 import { useActivePayments } from "../Premium";
 
