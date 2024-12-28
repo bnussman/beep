3935b87b332dacb61df8e63e5fbe574e4d6d68bf
Banks Nussman <banks@nussman.us>
2024-12-28 00:00:52 -0500

    refactor: move `Avatar` off of NativeWind

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,33 +2,34 @@ import React from "react";
 import { Image } from "react-native";
 import type { ImageProps } from "react-native";
 import AvatarImage from "../assets/avatar.png";
-import { VariantProps, cva } from "class-variance-authority";
 
-export const avatar = cva(["rounded-full"], {
-  variants: {
-    size: {
-      xs: "!size-12",
-      sm: "!size-14",
-      md: "!size-16",
-      lg: "!size-20",
-      xl: "!size-32",
-      "2xl": "!size-48",
-    },
-  },
-  defaultVariants: {
-    size: "md",
-  },
-});
+type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
 
-interface Props extends ImageProps, VariantProps<typeof avatar> {}
+const SIZE_MAP: Record<Size, number> = {
+  xs: 48,
+  sm: 56,
+  md: 64,
+  lg: 80,
+  xl: 128,
+  '2xl': 192,
+};
+
+interface Props extends ImageProps {
+  size: Size;
+}
 
 export function Avatar(props: Props) {
-  const { className, src, size, ...rest } = props;
+  const { className, src, size = 'md', ...rest } = props;
+
   return (
     <Image
       source={src ? { uri: src } : AvatarImage}
       defaultSource={AvatarImage}
-      className={avatar({ className, size })}
+      style={{
+        borderRadius: '50%',
+        width: SIZE_MAP[size],
+        height: SIZE_MAP[size]
+      }}
       {...rest}
     />
   );
