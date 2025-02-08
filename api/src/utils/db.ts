7ed2ad460c1455b67f308c9447d56a6f3905c873
Banks Nussman <banks@nussman.us>
2025-02-08 13:19:41 -0500

    chore: update db options

--- api/src/utils/db.ts
+++ api/src/utils/db.ts
@@ -2,13 +2,13 @@ import * as schema from '../../drizzle/schema';
 import type { ClientConfig } from 'pg';
 const { Client } = require("pg"); // we must use `require` because of Bun / Sentry / OpenTelementry weirdness
 import { drizzle } from "drizzle-orm/node-postgres";
-import { DB_URL, ENVIRONMENT, isDevelopment } from "./constants";
+import { DB_URL, isDevelopment } from "./constants";
 
 const options: ClientConfig = {
   connectionString: DB_URL,
-  ssl: ENVIRONMENT === "production" || ENVIRONMENT === "staging" ? {
+  ssl: !isDevelopment && ({
     rejectUnauthorized: false,
-  } : undefined,
+  }),
 };
 
 const queryClient = new Client(options);
