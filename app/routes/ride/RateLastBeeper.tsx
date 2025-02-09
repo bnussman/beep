8362e570ecc51af4dc6908a47148194ce0dfdcb5
Banks Nussman <banks@nussman.us>
2025-02-09 12:13:10 -0500

    change: clean up rate UI

--- app/routes/ride/RateLastBeeper.tsx
+++ app/routes/ride/RateLastBeeper.tsx
@@ -6,31 +6,30 @@ import { useUser } from "@/utils/useUser";
 import { useNavigation } from "@react-navigation/native";
 
 export function RateLastBeeper() {
-    const { user } = useUser();
-    const navigation = useNavigation();
-    const { data: beep } = trpc.rider.getLastBeepToRate.useQuery();
+  const { user } = useUser();
+  const navigation = useNavigation();
+  const { data: beep } = trpc.rider.getLastBeepToRate.useQuery();
 
-    if (!beep) {
-        return null;
-    }
-    
-    const otherUser = user?.id === beep?.rider_id ? beep.beeper : beep.rider;
+  if (!beep) {
+    return null;
+  }
 
-    return (
-        <Card
-            pressable
-            onPress={() => navigation.navigate("Rate", {
-                userId: otherUser.id,
-                beepId: beep.id,
-            })}
-            variant="outlined"
-            className="flex flex-row items-center justify-between px-4 py-2 mt-4 gap-4 ease-in ease-out duration-300"
-        >
-            <Text>⭐</Text>
-            <Text className="flex-shrink">
-              Rate Your Last Beep with <Text weight="black">{otherUser.first}</Text>
-            </Text>
-            <Avatar src={otherUser.photo ?? undefined} size="sm" />
-        </Card>
-    )
+  const otherUser = user?.id === beep?.rider_id ? beep.beeper : beep.rider;
+
+  return (
+    <Card
+      pressable
+      onPress={() => navigation.navigate("Rate", {
+        userId: otherUser.id,
+        beepId: beep.id,
+      })}
+      variant="outlined"
+      className="flex flex-row items-center justify-between px-4 py-2 mt-4 gap-4 ease-in ease-out duration-300"
+    >
+      <Text className="flex-shrink">
+        Rate Your Last Beep with <Text weight="black">{otherUser.first}</Text>
+      </Text>
+      <Avatar src={otherUser.photo ?? undefined} size="sm" />
+    </Card>
+  )
 }
