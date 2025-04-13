f77461538c2e56b2803e5ada725d3a9845522b6b
Banks Nussman <banks@nussman.us>
2025-04-12 22:39:52 -0400

    chore: imrpove photo dialog

--- website/src/components/PhotoDialog.tsx
+++ website/src/components/PhotoDialog.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Dialog, DialogContent } from "@mui/material";
+import { Dialog, DialogContent, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 
 interface Props {
   isOpen: boolean;
@@ -16,7 +17,28 @@ export function PhotoDialog(props: Props) {
 
   return (
     <Dialog open={isOpen} onClose={onClose}>
-      <DialogContent>
+      <IconButton
+        aria-label="close"
+        onClick={onClose}
+        sx={(theme) => ({
+          position: "absolute",
+          right: 8,
+          top: 8,
+          color: theme.palette.grey[400],
+        })}
+      >
+        <CloseIcon />
+      </IconButton>
+      <DialogContent
+        sx={{
+          p: 0,
+          minWidth: "300px",
+          minHeight: "200px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <img src={src} />
       </DialogContent>
     </Dialog>
