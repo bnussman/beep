fe47beb8812049e7c26776f8e3110bf13257b57d
Banks Nussman <banks@nussman.us>
2025-04-14 22:42:26 -0400

    chore: refactor send mass notification flow

--- website/src/routes/admin/notifications/index.tsx
+++ website/src/routes/admin/notifications/index.tsx
@@ -1,32 +1,20 @@
-import React, { useRef } from "react";
-import { useForm } from "react-hook-form";
-import { EmailIcon } from "@chakra-ui/icons";
-import { Error } from '../../../components/Error';
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import { createRoute } from "@tanstack/react-router";
 import { RouterInput, trpc } from "../../../utils/trpc";
 import { adminRoute } from "..";
 import {
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogContent,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogOverlay,
+  Alert,
+  TextField,
+  Typography,
   Button,
-  Code,
-  FormControl,
-  FormErrorMessage,
-  FormHelperText,
-  FormLabel,
-  Heading,
-  Input,
   Stack,
-  Textarea,
-  useDisclosure,
-  useToast
-} from "@chakra-ui/react";
+  Box,
+} from "@mui/material";
+import { useToast } from "@chakra-ui/react";
+import { SendNotificationConfirmationDialog } from "./SendNotificationConfirmationDialog";
 
-type SendNotifictionVariables = RouterInput['notification']['sendNotification'];
+type SendNotifictionVariables = RouterInput["notification"]["sendNotification"];
 
 export const notificationsRoute = createRoute({
   component: Notifications,
@@ -35,118 +23,118 @@ export const notificationsRoute = createRoute({
 });
 
 export function Notifications() {
-  const { isOpen, onClose, onOpen } = useDisclosure();
-  const cancelRef = useRef<any>();
   const toast = useToast();
 
-  const {
-    mutateAsync: sendNotification,
-    error,
-    isPending
-  } = trpc.notification.sendNotification.useMutation();
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
 
   const {
     handleSubmit,
-    register,
-    watch,
+    control,
     reset,
+    setError,
     formState: { errors, isSubmitting, isValid },
-  } = useForm<SendNotifictionVariables>({ mode: 'onChange' });
+  } = useForm<SendNotifictionVariables>({ mode: "onChange" });
 
-  const validationErrors = error?.data?.zodError?.fieldErrors;
-
-  const emailMatch = watch('emailMatch');
-
-  const onSubmit = handleSubmit((variables) => {
-    sendNotification(variables).then((numberOfNotificationsSend) => {
-      onClose();
-      toast({ title: `Sent notification to ${numberOfNotificationsSend} users.`})
-      reset();
+  const { mutateAsync: sendNotification } =
+    trpc.notification.sendNotification.useMutation({
+      onError(e) {
+        if (e.data?.zodError?.fieldErrors) {
+          for (const key in e.data?.zodError?.fieldErrors ?? {}) {
+            setError(key as keyof SendNotifictionVariables, {
+              message: e.data?.zodError?.fieldErrors?.[key]?.[0],
+            });
+          }
+        } else {
+          setError("root", { message: e.message });
+        }
+      },
     });
+
+  const onConfirm = handleSubmit(async (values) => {
+    setIsConfirmOpen(false);
+
+    try {
+      const sent = await sendNotification(values);
+
+      toast({
+        title: `Sent notification to ${sent} users.`,
+      });
+
+      reset();
+    } catch (error) {
+      // ...
+    }
   });
 
   return (
-    <>
-      <Stack spacing={4}>
-        <Heading>Notifications</Heading>
-        {error && <Error>{error.message}</Error>}
-        <FormControl isInvalid={Boolean(errors.title) || Boolean(validationErrors?.title)}>
-          <FormLabel>Title</FormLabel>
-          <Input
-            {...register('title', {
-              required: 'This is required',
-            })}
+    <Stack spacing={2}>
+      <Typography variant="h4" fontWeight="bold">
+        Notifications
+      </Typography>
+      <Typography>Use this tool to send mass notifications.</Typography>
+      <Alert severity="warning">
+        Please thoroughly review notifications before sending them. If no match
+        is specified, the notification will be sent to all users.
+      </Alert>
+      {errors.root?.message && (
+        <Alert severity="error">{errors.root.message}</Alert>
+      )}
+      <Controller
+        control={control}
+        name="title"
+        render={({ field, fieldState }) => (
+          <TextField
+            label="Title"
+            error={Boolean(fieldState.error?.message)}
+            helperText={fieldState.error?.message}
+            value={field.value}
+            onChange={field.onChange}
           />
-          <FormErrorMessage>
-            {errors.title && errors.title.message}
-            {validationErrors?.title && validationErrors?.title[0]}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={Boolean(errors.emailMatch) || Boolean(validationErrors?.emailMatch)}>
-          <FormLabel>Match</FormLabel>
-          <Input
+        )}
+      />
+      <Controller
+        control={control}
+        name="emailMatch"
+        render={({ field, fieldState }) => (
+          <TextField
+            label="Match"
+            error={Boolean(fieldState.error?.message)}
+            helperText={fieldState.error?.message}
+            value={field.value}
+            onChange={field.onChange}
             placeholder="%@appstate.edu"
-            {...register('emailMatch')}
           />
-          <FormHelperText>Leave this empty to send to all Beep App users.</FormHelperText>
-          <FormErrorMessage>
-            {errors.emailMatch && errors.emailMatch.message}
-            {validationErrors?.match && validationErrors?.match[0]}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isInvalid={Boolean(errors.body) || Boolean(validationErrors?.body)}>
-          <FormLabel>Body</FormLabel>
-          <Textarea
-            {...register('body', {
-              required: 'This is required',
-            })}
+        )}
+      />
+      <Controller
+        control={control}
+        name="body"
+        render={({ field, fieldState }) => (
+          <TextField
+            label="Body"
+            error={Boolean(fieldState.error?.message)}
+            helperText={fieldState.error?.message}
+            value={field.value}
+            onChange={field.onChange}
+            multiline
+            rows={2}
           />
-          <FormErrorMessage>
-            {errors.body && errors.body.message}
-            {validationErrors?.body && validationErrors?.body[0]}
-          </FormErrorMessage>
-        </FormControl>
-      </Stack>
-      <Button
-        mt={4}
-        colorScheme="purple"
-        onClick={onOpen}
-        rightIcon={<EmailIcon />}
-        isDisabled={!isValid}
-        isLoading={isSubmitting || isPending}
-      >
-        Send
-      </Button>
-      <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
-              Send Notification
-            </AlertDialogHeader>
-            <AlertDialogBody>
-              Are you sure? {emailMatch ? <>This will be sent to all user having an email that matches <Code>{emailMatch}</Code></> : 'This will be sent to all Beep App users.'}
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button
-                ml={3}
-                colorScheme="blue"
-                type="submit"
-                onClick={onSubmit}
-                isLoading={isSubmitting || isPending}
-              >
-                Send
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
-    </>
+        )}
+      />
+      <Box display="flex" justifyContent="flex-end">
+        <Button
+          onClick={() => setIsConfirmOpen(true)}
+          variant="contained"
+          loading={isSubmitting}
+        >
+          Send
+        </Button>
+      </Box>
+      <SendNotificationConfirmationDialog
+        open={isConfirmOpen}
+        onClose={() => setIsConfirmOpen(false)}
+        onConfirm={onConfirm}
+      />
+    </Stack>
   );
 }
