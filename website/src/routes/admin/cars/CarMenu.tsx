c240a3a49f84e7152c3fd2573f1c2880f563c44e
Banks Nussman <banks@nussman.us>
2025-04-19 17:57:08 -0400

    chore: improve car menu

--- website/src/routes/admin/cars/CarMenu.tsx
+++ website/src/routes/admin/cars/CarMenu.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { IconButton, Menu, MenuItem } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { trpc } from "../../../utils/trpc";
+import { useNotifications } from "@toolpad/core";
 
 interface Props {
   carId: string;
@@ -10,24 +11,29 @@ interface Props {
 
 export function CarMenu(props: Props) {
   const { carId, onDelete } = props;
+
+  const notifications = useNotifications();
+  const utils = trpc.useUtils();
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
   const open = Boolean(anchorEl);
-
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
-
   const handleClose = () => {
     setAnchorEl(null);
   };
 
-  const utils = trpc.useUtils();
 
   const { mutateAsync: updateCar } = trpc.car.updateCar.useMutation({
     onSuccess() {
       utils.car.cars.invalidate();
+      notifications.show("Sucessfully made car default for user", { severity: 'success' });
+      handleClose();
     },
+    onError(error) {
+      notifications.show(error.message, { severity: 'error' });
+    }
   });
 
   return (
