6cc711464e7583331fe27eda76e1d34ce9c5db1c
Banks Nussman <banks@nussman.us>
2025-04-27 16:20:59 -0400

    fix typecheck

--- api/src/routers/redis.ts
+++ api/src/routers/redis.ts
@@ -4,6 +4,6 @@ import { adminProcedure, router } from "../utils/trpc";
 export const redisRouter = router({
   channels: adminProcedure
     .query(async () => {
-      return await publishClient.pubsub('CHANNELS');
+      return await publishClient.pubsub('CHANNELS') as string[];
     })
 });
