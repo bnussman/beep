48e469e4b36e2cf5087a93f49695abeb9cc29bcf
Banks Nussman <banks@nussman.us>
2025-05-28 19:42:21 -0400

    fix: use new s3 env var

--- api/src/utils/s3.ts
+++ api/src/utils/s3.ts
@@ -1,10 +1,9 @@
-import { S3_ACCESS_KEY_ID, S3_ACCESS_KEY_SECRET, S3_ENDPOINT } from "./constants";
+import { S3_ACCESS_KEY_ID, S3_ACCESS_KEY_SECRET, S3_BUCKET, S3_ENDPOINT } from "./constants";
 import { S3Client } from 'bun';
 
 export const s3 = new S3Client({
   endpoint: S3_ENDPOINT,
   secretAccessKey: S3_ACCESS_KEY_SECRET,
   accessKeyId: S3_ACCESS_KEY_ID,
-  region: 'beep',
-  bucket: "beep",
+  bucket: S3_BUCKET,
 });
