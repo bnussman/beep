668cf15cdad008170a1e96e31fd91bc9b9d885d3
Banks Nussman <banks@nussman.us>
2025-06-15 21:01:38 -0400

    protect

--- api/src/utils/trpc.ts
+++ api/src/utils/trpc.ts
@@ -4,8 +4,8 @@ import { ZodError } from 'zod';
 import { AppRouter } from '..';
 import { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
 import { db } from './db';
-import { token } from '../../drizzle/schema';
-import { eq } from 'drizzle-orm';
+import { beep, token } from '../../drizzle/schema';
+import { and, eq, getOperators, or } from 'drizzle-orm';
 
 /**
  * Initialization of tRPC backend
@@ -45,7 +45,56 @@ const sentryMiddleware = t.middleware((opts) => {
   return opts.next();
 });
 
-export const publicProcedure = t.procedure.use(sentryMiddleware);
+const PROTECTED_FIELDS = ['phone', 'email', 'location', 'password', 'passwordType', 'pushToken'];
+
+async function getProtectedData(obj: any, ctx: Context) {
+  if (!ctx.user) {
+    console.log('aborting due to no user in ctx')
+    return;
+  }
+  if (ctx.user.role === 'admin') {
+    return;
+  }
+  if (typeof obj === 'object') {
+    const keys = Object.keys(obj);
+    if (keys.includes('id') && keys.some(k => PROTECTED_FIELDS.includes(k))) {
+      const hasPermission = await db.query.beep.findFirst({ 
+        where: and(
+          eq(beep.status, 'complete'),
+          or(
+            and(eq(beep.rider_id, ctx.user.id), eq(beep.beeper_id, obj['id'])),
+            and(eq(beep.rider_id, obj['id']), eq(beep.beeper_id, ctx.user.id)),
+          )
+        )
+      });
+
+      if (!hasPermission) {
+        console.log('no permission')
+        for(const key of keys)  {
+          if (PROTECTED_FIELDS.includes(key)) {
+            obj[key] = null;
+          }
+        }
+      }
+    }
+  }
+
+  for (const key in obj) {
+    if (typeof obj[key] === 'object' && obj[key] !== null)  {
+      await getProtectedData(obj[key], ctx);
+    }
+  }
+}
+
+const protectMiddleware = t.middleware(async (opts) => {
+  const result = await opts.next();
+  if (result.ok && result.data) {
+    await getProtectedData(result.data, opts.ctx) 
+  }
+  return result;
+});
+
+export const publicProcedure = t.procedure.use(sentryMiddleware).use(protectMiddleware);
 
 export const authedProcedure = publicProcedure.use(function isAuthed(opts) {
   const { ctx } = opts;
