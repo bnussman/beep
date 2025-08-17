71ce79596ad08810ff76fa3b2fe9295d41a1a347
Banks Nussman <banks@nussman.us>
2025-08-17 18:06:20 -0400

    chore: use new photon client

--- api/src/logic/location.ts
+++ api/src/logic/location.ts
@@ -1,28 +1,21 @@
-import { Photon } from "../utils/photon";
+import { photon } from "@banksnussman/photon";
 import { user } from "../../drizzle/schema";
 
 export async function getCoordinatesFromAddress(
   address: string,
   bias: (typeof user.$inferSelect)["location"],
 ) {
-  const queryParams = new URLSearchParams({
-    q: address,
-    zoom: "10",
+  const { data, error } = await photon.GET("/api", {
+    params: {
+      query: {
+        q: address,
+        // zoom: "10",
+        ...(bias && { lat: bias.latitude, lon: bias.longitude }),
+      },
+    },
   });
 
-  if (bias) {
-    queryParams.set("lat", bias.latitude.toString());
-    queryParams.set("lon", bias.longitude.toString());
-  }
-
-  const result = await fetch(
-    `https://photon.nussman.us/api?${queryParams.toString()}`,
-  );
-
-  const data: Photon["/api"]["get"]["responses"]["200"]["content"]["application/json"] =
-    await result.json();
-
-  if (!data.features[0]) {
+  if (error || !data.features[0]) {
     return null;
   }
 
