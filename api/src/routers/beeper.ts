874e489ff027f9836d4fbf2b9811d2c7f495cae5
Banks Nussman <banks@nussman.us>
2025-09-07 23:28:35 -0400

    refactor: rework more beep logic

--- api/src/routers/beeper.ts
+++ api/src/routers/beeper.ts
@@ -1,4 +1,4 @@
-import * as Sentry from '@sentry/bun';
+import * as Sentry from "@sentry/bun";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { authedProcedure, router } from "../utils/trpc";
@@ -7,32 +7,38 @@ import { eq } from "drizzle-orm";
 import { beep, user } from "../../drizzle/schema";
 import { sendNotification } from "../utils/notifications";
 import { pubSub } from "../utils/pubsub";
-import { getBeeperQueue, getProtectedBeeperQueue, getQueueSize, getRiderBeepFromBeeperQueue } from "../logic/beep";
+import {
+  getBeeperQueue,
+  getProtectedBeeperQueue,
+  getQueueSize,
+  getRiderBeepFromBeeperQueue,
+} from "../logic/beep";
 
 export const beeperRouter = router({
   queue: authedProcedure
     .input(z.string().optional())
     .query(async ({ input, ctx }) => {
-      if (input && input !== ctx.user.id && ctx.user.role !== 'admin') {
+      if (input && input !== ctx.user.id && ctx.user.role !== "admin") {
         throw new TRPCError({
           code: "UNAUTHORIZED",
-          message: "You must be an admin to view other user's queue."
+          message: "You must be an admin to view other user's queue.",
         });
       }
 
-      const queue = await getBeeperQueue(input ?? ctx.user.id)
+      const queue = await getBeeperQueue(input ?? ctx.user.id);
 
       return getProtectedBeeperQueue(queue);
     }),
   watchQueue: authedProcedure
     .input(z.string().optional())
-    .subscription(async function*({ ctx, input, signal }) {
+    .subscription(async function* ({ ctx, input, signal }) {
       const id = input ?? ctx.user.id;
 
-      if (ctx.user.role === 'user' && input && input !== ctx.user.id) {
+      if (ctx.user.role === "user" && input && input !== ctx.user.id) {
         throw new TRPCError({
-          code: 'UNAUTHORIZED',
-          message: "You do not have permission to subscribe to another user's queue"
+          code: "UNAUTHORIZED",
+          message:
+            "You do not have permission to subscribe to another user's queue",
         });
       }
 
@@ -49,22 +55,30 @@ export const beeperRouter = router({
 
       yield getProtectedBeeperQueue(queue);
 
-      const eventSource = pubSub.subscribe('queue', id);
+      const eventSource = pubSub.subscribe("queue", id);
 
       for await (const { queue } of eventSource) {
         if (signal?.aborted) return;
         yield getProtectedBeeperQueue(queue);
       }
-
-  }),
+    }),
   updateBeep: authedProcedure
     .input(
       z.object({
         beepId: z.string(),
         data: z.object({
-          status: z.enum(["canceled", "denied", "waiting", "accepted", "on_the_way", "here", "in_progress", "complete"])
-        })
-      })
+          status: z.enum([
+            "canceled",
+            "denied",
+            "waiting",
+            "accepted",
+            "on_the_way",
+            "here",
+            "in_progress",
+            "complete",
+          ]),
+        }),
+      }),
     )
     .mutation(async ({ input, ctx }) => {
       const queue = await getBeeperQueue(ctx.user.id);
@@ -74,17 +88,23 @@ export const beeperRouter = router({
       if (!queueEntry) {
         throw new TRPCError({
           code: "NOT_FOUND",
-          message: "Can't find that beep."
+          message: "Can't find that beep.",
         });
       }
 
       const isAcceptingOrDenying =
         input.data.status === "accepted" || input.data.status === "denied";
 
-      if (isAcceptingOrDenying && queue.filter((entry) => entry.start < queueEntry.start && entry.status === "waiting").length !== 0) {
+      if (
+        isAcceptingOrDenying &&
+        queue.filter(
+          (entry) =>
+            entry.start < queueEntry.start && entry.status === "waiting",
+        ).length !== 0
+      ) {
         throw new TRPCError({
           code: "BAD_REQUEST",
-          message: "You must respond to the rider who first joined your queue."
+          message: "You must respond to the rider who first joined your queue.",
         });
       }
 
@@ -102,8 +122,12 @@ export const beeperRouter = router({
           .where(eq(user.id, ctx.user.id));
       }
 
-      if (input.data.status === "denied" || input.data.status === "complete" || input.data.status === "canceled") {
-        pubSub.publish('ride', queueEntry.rider_id, { ride: null });
+      if (
+        input.data.status === "denied" ||
+        input.data.status === "complete" ||
+        input.data.status === "canceled"
+      ) {
+        pubSub.publish("ride", queueEntry.rider_id, { ride: null });
 
         await db
           .update(user)
@@ -122,35 +146,35 @@ export const beeperRouter = router({
             sendNotification({
               to: queueEntry.rider.pushToken,
               title: `${ctx.user.first} ${ctx.user.last} has canceled your beep 🚫`,
-              body: "Open your app to find a new beep"
+              body: "Open your app to find a new beep",
             });
             break;
           case "denied":
             sendNotification({
               to: queueEntry.rider.pushToken,
               title: `${ctx.user.first} ${ctx.user.last} has denied your beep request 🚫`,
-              body: "Open your app to find a different beeper"
+              body: "Open your app to find a different beeper",
             });
             break;
           case "accepted":
             sendNotification({
               to: queueEntry.rider.pushToken,
               title: `${ctx.user.first} ${ctx.user.last} has accepted your beep request ✅`,
-              body: "You will receive another notification when they are on their way to pick you up"
+              body: "You will receive another notification when they are on their way to pick you up",
             });
             break;
           case "on_the_way":
             sendNotification({
               to: queueEntry.rider.pushToken,
               title: `${ctx.user.first} ${ctx.user.last} is on their way 🚕`,
-              body: `Your beeper is on their way in a ${queueEntry.beeper.cars[0]?.color} ${queueEntry.beeper.cars[0]?.make} ${queueEntry.beeper.cars[0]?.model}`
+              body: `Your beeper is on their way in a ${queueEntry.beeper.cars[0]?.color} ${queueEntry.beeper.cars[0]?.make} ${queueEntry.beeper.cars[0]?.model}`,
             });
             break;
           case "here":
             sendNotification({
               to: queueEntry.rider.pushToken,
               title: `${ctx.user.first} ${ctx.user.last} is here 📍`,
-              body: `Look for a ${queueEntry.beeper.cars[0]?.color} ${queueEntry.beeper.cars[0]?.make} ${queueEntry.beeper.cars[0]?.model}`
+              body: `Look for a ${queueEntry.beeper.cars[0]?.color} ${queueEntry.beeper.cars[0]?.make} ${queueEntry.beeper.cars[0]?.model}`,
             });
             break;
           case "in_progress":
@@ -160,18 +184,27 @@ export const beeperRouter = router({
             // Beep is complete.
             break;
           default:
-            Sentry.captureException("Our beeper's state notification switch statement reached a point that is should not have");
+            Sentry.captureException(
+              "Our beeper's state notification switch statement reached a point that is should not have",
+            );
         }
       }
 
-      const newQueue = queue.filter((beep) => beep.status !== "complete" && beep.status !== 'denied' && beep.status !== "canceled");
+      const newQueue = queue.filter(
+        (beep) =>
+          beep.status !== "complete" &&
+          beep.status !== "denied" &&
+          beep.status !== "canceled",
+      );
 
-      pubSub.publish('queue', ctx.user.id, { queue: newQueue });
+      pubSub.publish("queue", ctx.user.id, { queue: newQueue });
 
       for (const entry of newQueue) {
-        pubSub.publish('ride', entry.rider_id, { ride: getRiderBeepFromBeeperQueue(entry.rider_id, newQueue) });
+        pubSub.publish("ride", entry.rider_id, {
+          ride: getRiderBeepFromBeeperQueue(entry.rider_id, newQueue),
+        });
       }
 
-      return newQueue;
-    })
+      return getProtectedBeeperQueue(newQueue);
+    }),
 });
