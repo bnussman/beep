5970fa558f284b22cd11b18ed4ea874697909c63
Banks Nussman <banks@nussman.us>
2025-09-08 23:23:29 -0400

    chore: clean up util functions in API

--- api/src/logic/location.ts
+++ api/src/logic/location.ts
@@ -27,3 +27,29 @@ export async function getCoordinatesFromAddress(
 
   return { latitude, longitude };
 }
+
+export function getDistance(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number,
+): number {
+  const R = 6371;
+  const dLat = deg2rad(lat2 - lat1);
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const d = R * c;
+  return d * 0.621371;
+}
+
+function deg2rad(deg: number): number {
+  return deg * (Math.PI / 180);
+}
--- api/src/routers/rider.ts
+++ api/src/routers/rider.ts
@@ -13,6 +13,7 @@ import {
   inProgressBeep,
 } from "../logic/beep";
 import { DEFAULT_LOCATION_RADIUS } from "../utils/constants";
+import { getDistance } from "../logic/location";
 
 export const riderRouter = router({
   beepers: verifiedProcedure
@@ -456,29 +457,3 @@ export const riderRouter = router({
     return mostRecentCompletedBeep;
   }),
 });
-
-export function getDistance(
-  lat1: number,
-  lon1: number,
-  lat2: number,
-  lon2: number,
-): number {
-  const R = 6371;
-  const dLat = deg2rad(lat2 - lat1);
-  const dLon = deg2rad(lon2 - lon1);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) *
-      Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-  const d = R * c;
-  return d * 0.621371;
-}
-
-function deg2rad(deg: number): number {
-  return deg * (Math.PI / 180);
-}
