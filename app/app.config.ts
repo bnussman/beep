f85052f1e228f241cb50d4b5a8b32ba18b3e192d
Banks Nussman <banks@nussman.us>
2025-09-19 22:46:57 -0400

    Revert "fix: attempt to fix Android location bugs"
    This reverts commit 44ceaec3ff1aa01609f81079471eb633222d12b3.

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,4 +1,4 @@
-import type { ExpoConfig } from "expo/config";
+import type { ExpoConfig } from 'expo/config';
 
 const config: ExpoConfig = {
   experiments: {
@@ -23,7 +23,7 @@ const config: ExpoConfig = {
     fallbackToCacheTimeout: 10_000,
   },
   runtimeVersion: {
-    policy: "sdkVersion",
+    policy: "sdkVersion"
   },
   extra: {
     eas: {
@@ -38,18 +38,11 @@ const config: ExpoConfig = {
   plugins: [
     ["expo-notifications"],
     [
-      "expo-location",
-      {
-        isIosBackgroundLocationEnabled: true,
-        isAndroidBackgroundLocationEnabled: true,
-      },
-    ],
-    [
       "@sentry/react-native/expo",
       {
         project: "app",
-        organization: "ian-banks-llc",
-      },
+        organization: "ian-banks-llc"
+      }
     ],
   ],
   ios: {
@@ -69,9 +62,9 @@ const config: ExpoConfig = {
       googleMapsApiKey: "AIzaSyDpCZoq8gSeOxpqHzk1VBoC3XgajTcSjf0",
     },
     icon: {
-      light: "./assets/icon.png",
-      dark: "./assets/icon-transparent.png",
-      tinted: "./assets/icon-tinted.png",
+      light: './assets/icon.png',
+      dark: './assets/icon-transparent.png',
+      tinted: './assets/icon-tinted.png'
     },
     splash: {
       image: "./assets/splash.png",
@@ -81,7 +74,7 @@ const config: ExpoConfig = {
         image: "./assets/splash.png",
         resizeMode: "cover",
         backgroundColor: "#000000",
-      },
+      }
     },
   },
   android: {
@@ -92,7 +85,6 @@ const config: ExpoConfig = {
       "ACCESS_COARSE_LOCATION",
       "ACCESS_FINE_LOCATION",
       "ACCESS_BACKGROUND_LOCATION",
-      "FOREGROUND_SERVICE_LOCATION",
       "FOREGROUND_SERVICE",
       "VIBRATE",
     ],
@@ -109,7 +101,7 @@ const config: ExpoConfig = {
         image: "./assets/splash.png",
         resizeMode: "cover",
         backgroundColor: "#000000",
-      },
+      }
     },
   },
   assetBundlePatterns: ["**/*"],
