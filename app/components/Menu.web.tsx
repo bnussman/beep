aec099e9d9d74811e4c693a88e3e8608e0243b7c
Banks Nussman <banks@nussman.us>
2025-09-21 21:34:11 -0400

    fix: improve context menu usability on web

--- app/components/Menu.web.tsx
+++ app/components/Menu.web.tsx
@@ -1,15 +1,23 @@
-import { useId } from "react";
+import { useId, useRef } from "react";
 import { MenuProps } from "./Menu";
 
 export function Menu(props: MenuProps) {
   const id = useId();
+  const ref = useRef<HTMLDivElement>(null);
 
   return (
     <>
-      <button style={{ all: "unset" }} popoverTarget={id}>
+      <button
+        style={{ all: "unset", cursor: "pointer" }}
+        popoverTarget={id}
+        onContextMenu={(e) => {
+          e.preventDefault();
+          ref.current?.showPopover();
+        }}
+      >
         {props.trigger}
       </button>
-      <div popover="" popoverTargetAction="toggle" id={id}>
+      <div popover="" popoverTargetAction="toggle" id={id} ref={ref}>
         {props.options.map((option, index) => (
           <button role="menuitem" key={index} onClick={option.onClick}>
             {option.title}
