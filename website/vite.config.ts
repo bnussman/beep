1b28c989094859236cf6a6ac1a18d1b7a9ff536f
Banks Nussman <banks@nussman.us>
2025-09-28 17:40:17 -0400

    build: use rolldown and react compiler

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
+});
