7dbc14d489f7477388d9129ef30b5b7a802cc738
Banks Nussman <banks@nussman.us>
2025-10-21 21:46:33 -0400

    Revert "fix: workaround glitchy pressable ui"
    This reverts commit f8816e5997b38b2788d1a47524339c70c1ca1152.

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,7 +1,6 @@
 import { Theme, useTheme } from "@/utils/theme";
 import React from "react";
-import { PressableProps, StyleSheet } from "react-native";
-import { Pressable } from "./Pressable";
+import { Pressable, PressableProps, StyleSheet } from "react-native";
 
 interface Props extends PressableProps {
   /**
@@ -12,44 +11,43 @@ interface Props extends PressableProps {
   pressable?: boolean;
   /**
    * Variant for the card
-   *
+   * 
    * @default 'filled'
    */
-  variant?: "outlined" | "filled";
+  variant?: 'outlined' | 'filled';
 }
 
 export function Card(props: Props) {
-  const { pressable, variant = "filled", ...rest } = props;
+  const { pressable, variant = 'filled', ...rest } = props;
 
-  const theme = useTheme();
+  const theme  = useTheme();
   const styles = createStyle(theme);
 
   return (
     <Pressable
       {...rest}
-      style={(options) => [
-        styles.card,
-        variant === "outlined" && {
-          backgroundColor: "transparent",
-        },
-        pressable &&
-          options.pressed && {
-            backgroundColor:
-              theme.components.button.primary.pressed.backgroundColor,
+      style={(options) => (
+        [
+          styles.card,
+          variant === 'outlined' && {
+            backgroundColor: 'transparent',
           },
-        typeof rest.style === "function" ? rest.style(options) : rest.style,
-      ]}
+          pressable && options.pressed && {
+            backgroundColor: theme.components.button.primary.pressed.backgroundColor,
+          },
+          typeof rest.style === 'function' ? rest.style(options) : rest.style
+        ]
+      )}
     />
   );
 }
 
-const createStyle = (theme: Theme) =>
-  StyleSheet.create({
-    card: {
-      backgroundColor: theme.components.card.backgroundColor,
-      borderColor: theme.components.card.borderColor,
-      borderRadius: 12,
-      borderWidth: theme.name === "light" ? 1.5 : 1,
-      padding: 16,
-    },
-  });
+const createStyle = (theme: Theme) => StyleSheet.create({
+  card: {
+    backgroundColor: theme.components.card.backgroundColor,
+    borderColor: theme.components.card.borderColor,
+    borderRadius: 12,
+    borderWidth: theme.name === "light" ? 1.5 : 1,
+    padding: 16,
+  },
+});
