2b1288ad4c5834cd9abd6ada3bd12db708ea5b65
Banks Nussman <banks@nussman.us>
2026-01-21 19:54:58 -0500

    save

--- orpc/src/utils/trpc.ts
+++ orpc/src/utils/trpc.ts
@@ -1,210 +1,15 @@
 import * as Sentry from "@sentry/bun";
-import { TRPCError, inferRouterInputs, initTRPC } from "@trpc/server";
-import z, { ZodError } from "zod";
-import { AppRouter } from "..";
-import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import { db } from "./db";
 import { isAcceptedBeepNew } from "../logic/beep";
 import { createLock, IoredisAdapter } from "redlock-universal";
 import { redis } from "./redis";
-
-/**
- * Initialization of tRPC backend
- * Should be done only once per backend!
- */
-const t = initTRPC.context<Context>().create({
-  errorFormatter(opts) {
-    const { shape, error } = opts;
-
-    return {
-      ...shape,
-      data: {
-        ...shape.data,
-        fieldErrors:
-          error.code === "BAD_REQUEST" && error.cause instanceof ZodError
-            ? (z.flattenError(error.cause).fieldErrors as Record<
-                string,
-                string[]
-              >)
-            : null,
-      },
-    };
-  },
-});
-
-export type RouterInput = inferRouterInputs<AppRouter>;
-
-/**
- * Export reusable router and procedure helpers
- * that can be used throughout the router
- */
-export const router = t.router;
-
-const sentryMiddleware = t.middleware((opts) => {
-  Sentry.getCurrentScope().setTransactionName(`${opts.type} ${opts.path}`);
-  const activeSpan = Sentry.getActiveSpan();
-  if (activeSpan) {
-    Sentry.updateSpanName(activeSpan, `${opts.type} ${opts.path}`);
-  }
-
-  return opts.next();
-});
-
-export const publicProcedure = t.procedure.use(sentryMiddleware);
-
-export const authedProcedure = publicProcedure.use(function isAuthed(opts) {
-  const { ctx } = opts;
-
-  if (!ctx.user || !ctx.token) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
-
-  return opts.next({ ctx });
-});
-
-export const verifiedProcedure = authedProcedure.use(function isVerified(opts) {
-  const { ctx } = opts;
-
-  if (!ctx.user.isStudent || !ctx.user.isEmailVerified) {
-    throw new TRPCError({
-      code: "UNAUTHORIZED",
-      message: "Your edu email must be verified.",
-    });
-  }
-
-  return opts.next({ ctx });
-});
-
-export const adminProcedure = authedProcedure.use(function isAdmin(opts) {
-  const { ctx } = opts;
-
-  if (ctx.user.role !== "admin") {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  }
-
-  return opts.next({ ctx });
-});
-
-export const mustHaveBeenInAcceptedBeep = t.procedure
-  .input(z.string())
-  .use(async function checkIfUserHasBeenInAnAcceptedBeep(opts) {
-    if (!opts.ctx.user) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-
-    if (opts.ctx.user.role === "admin" || opts.input === opts.ctx.user.id) {
-      return opts.next(opts);
-    }
-
-    const acceptedOrCompleteBeep = await db.query.beep.findFirst({
-      where: {
-        AND: [
-          { OR: [isAcceptedBeepNew, { status: "complete" }] },
-          {
-            OR: [
-              {
-                AND: [
-                  { rider_id: opts.ctx.user.id },
-                  { beeper_id: opts.input },
-                ],
-              },
-              {
-                AND: [
-                  { rider_id: opts.input },
-                  { beeper_id: opts.ctx.user.id },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    });
-
-    if (!acceptedOrCompleteBeep) {
-      throw new TRPCError({
-        code: "FORBIDDEN",
-        message:
-          "You be in an accepted beep with that user or have completed a beep with them in the past to perform this action.",
-      });
-    }
-
-    return opts.next(opts);
-  });
-
-export const mustBeInAcceptedBeep = t.procedure
-  .input(z.string())
-  .use(async function checkIfUserIsInAnAcceptedBeep(opts) {
-    if (!opts.ctx.user) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-
-    if (opts.ctx.user.role === "admin" || opts.input === opts.ctx.user.id) {
-      return opts.next(opts);
-    }
-
-    const acceptedBeep = await db.query.beep.findFirst({
-      where: {
-        AND: [
-          isAcceptedBeepNew,
-          {
-            OR: [
-              {
-                AND: [
-                  { rider_id: opts.ctx.user.id },
-                  { beeper_id: opts.input },
-                ],
-              },
-              {
-                AND: [
-                  { rider_id: opts.input },
-                  { beeper_id: opts.ctx.user.id },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    });
-
-    if (!acceptedBeep) {
-      throw new TRPCError({
-        code: "FORBIDDEN",
-        message:
-          "You must be in an accepted beep with the user to perform this action.",
-      });
-    }
-
-    return opts.next(opts);
-  });
-
-export const withLock = t.middleware(async function handleLock(opts) {
-  if (!opts.ctx.user) {
-    throw new Error(
-      "This middleware should only be used for authenticated procedures.",
-    );
-  }
-
-  const lock = createLock({
-    adapter: new IoredisAdapter(redis),
-    key: `${opts.path}-${opts.ctx.user.id}`,
-    ttl: 5_000,
-  });
-
-  const handle = await lock.acquire();
-
-  const result = await opts.next({ ctx: opts.ctx });
-
-  await lock.release(handle);
-
-  return result;
-});
+import { os, ORPCError } from "@orpc/server";
 
 export async function createContext(
-  data: Omit<FetchCreateContextFnOptions, "resHeaders">,
+  request: Request
 ) {
-  const bearerToken =
-    data.req?.headers.get("authorization")?.split(" ")[1] ??
-    data.info?.connectionParams?.token;
+  const bearerToken = request.headers.get("authorization")?.split(" ")[1]
+
 
   if (!bearerToken) {
     return {};
@@ -231,4 +36,145 @@ export async function createContext(
   return { user: session.user, token: session };
 }
 
+
 export type Context = Awaited<ReturnType<typeof createContext>>;
+
+export const o = os.$context<Context>()
+
+export const authedProcedure = o.use(function isAuthed({ next, context }) {
+  if (!context.user || !context.token) {
+    throw new ORPCError("UNAUTHORIZED");
+  }
+
+  return next({ context });
+});
+
+export const verifiedProcedure = authedProcedure.use(function isVerified({ context, next }) {
+  if (!context.user.isStudent || !context.user.isEmailVerified) {
+    throw new ORPCError("UNAUTHORIZED", {
+      message: "Your edu email must be verified.",
+    });
+  }
+
+  return next({ context });
+});
+
+export const adminProcedure = authedProcedure.use(function isAdmin(opts) {
+  const { context } = opts;
+
+  if (context.user.role !== "admin") {
+    throw new ORPCError("UNAUTHORIZED");
+  }
+
+  return opts.next({ context });
+});
+
+export const mustHaveBeenInAcceptedBeep = o.middleware(async function checkIfUserHasBeenInAnAcceptedBeep(opts, userId: string) {
+    if (!opts.context.user) {
+      throw new ORPCError("UNAUTHORIZED");
+    }
+
+    if (opts.context.user.role === "admin" || userId === opts.context.user.id) {
+      return opts.next(opts);
+    }
+
+    const acceptedOrCompleteBeep = await db.query.beep.findFirst({
+      where: {
+        AND: [
+          { OR: [isAcceptedBeepNew, { status: "complete" }] },
+          {
+            OR: [
+              {
+                AND: [
+                  { rider_id: opts.context.user.id },
+                  { beeper_id: userId },
+                ],
+              },
+              {
+                AND: [
+                  { rider_id: userId },
+                  { beeper_id: opts.context.user.id },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    });
+
+    if (!acceptedOrCompleteBeep) {
+      throw new ORPCError("FORBIDDEN", {
+        message:
+          "You be in an accepted beep with that user or have completed a beep with them in the past to perform this action.",
+      });
+    }
+
+    return opts.next(opts);
+  });
+
+export const mustBeInAcceptedBeep = o.middleware(async function checkIfUserIsInAnAcceptedBeep(opts, userId: string) {
+    if (!opts.context.user) {
+      throw new ORPCError("UNAUTHORIZED");
+    }
+
+    if (opts.context.user.role === "admin" || userId === opts.context.user.id) {
+      return opts.next(opts);
+    }
+
+    const acceptedBeep = await db.query.beep.findFirst({
+      where: {
+        AND: [
+          isAcceptedBeepNew,
+          {
+            OR: [
+              {
+                AND: [
+                  { rider_id: opts.context.user.id },
+                  { beeper_id: userId },
+                ],
+              },
+              {
+                AND: [
+                  { rider_id: userId },
+                  { beeper_id: opts.context.user.id },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    });
+
+    if (!acceptedBeep) {
+      throw new ORPCError("FORBIDDEN", {
+        message:
+          "You must be in an accepted beep with the user to perform this action.",
+      });
+    }
+
+    return opts.next(opts);
+  });
+
+export const withLock = o.middleware(async function handleLock(opts) {
+  if (!opts.context.user) {
+    throw new Error(
+      "This middleware should only be used for authenticated procedures.",
+    );
+  }
+
+  const lock = createLock({
+    adapter: new IoredisAdapter(redis),
+    key: `${opts.path}-${opts.context.user.id}`,
+    ttl: 5_000,
+  });
+
+  const handle = await lock.acquire();
+
+  const result = await opts.next(opts);
+
+  await lock.release(handle);
+
+  return result;
+});
+
+
