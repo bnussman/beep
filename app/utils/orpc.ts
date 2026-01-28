896fac7adc40147c6d1f8af385120b76680db7cf
Banks Nussman <banks@nussman.us>
2026-01-27 21:21:53 -0500

    fix: react native file upload with oRPC

--- app/utils/orpc.ts
+++ app/utils/orpc.ts
@@ -1,14 +1,19 @@
 import * as Sentry from "@sentry/react-native";
-import Constants from 'expo-constants';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createORPCClient, onError } from '@orpc/client'
-import { RPCLink } from '@orpc/client/fetch'
-import { createTanstackQueryUtils } from '@orpc/tanstack-query'
-import { useQuery } from '@tanstack/react-query';
-import { isWeb } from './constants';
-import { fetch } from 'expo/fetch';
-import type { AppRouter } from '../../orpc/src/index'
-import type { AsyncIteratorClass, InferRouterInputs, InferRouterOutputs, RouterClient } from '@orpc/server'
+import Constants from "expo-constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createORPCClient, onError } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
+import { createTanstackQueryUtils } from "@orpc/tanstack-query";
+import { useQuery } from "@tanstack/react-query";
+import { isWeb } from "./constants";
+import { fetch as expoFetch } from "expo/fetch";
+import type { AppRouter } from "../../orpc/src/index";
+import type {
+  AsyncIteratorClass,
+  InferRouterInputs,
+  InferRouterOutputs,
+  RouterClient,
+} from "@orpc/server";
 
 function getLocalIP() {
   if (isWeb) {
@@ -47,16 +52,28 @@ const url = __DEV__ ? `http://${ip}:3001` : "https://orpc.ridebeep.app";
 
 const link = new RPCLink({
   url,
-  async fetch(request, init) {
-    const resp = await fetch(request.url, {
+  async fetch(request, init, context) {
+    console.log(request, init, context);
+    if (request._bodyFormData) {
+      const resp = await fetch(request.url, {
+        body: request._bodyFormData,
+        headers: request.headers,
+        method: request.method,
+        signal: request.signal,
+        ...init,
+      });
+
+      return resp;
+    }
+    const resp = await expoFetch(request.url, {
       body: await request.blob(),
       headers: request.headers,
       method: request.method,
       signal: request.signal,
       ...init,
-    })
+    });
 
-    return resp
+    return resp;
   },
   async headers() {
     const token = await getAuthToken();
@@ -67,22 +84,21 @@ const link = new RPCLink({
   },
   interceptors: [
     onError((error) => {
-      console.error(error)
-    })
+      console.error(error);
+    }),
   ],
 });
 
 export const client: RouterClient<AppRouter> = createORPCClient(link);
 export const orpc = createTanstackQueryUtils(client);
-export const useUser = () => useQuery(
-  orpc.user.updates.experimental_liveOptions({
-    enabled: false,
-  })
-)
+export const useUser = () =>
+  useQuery(
+    orpc.user.updates.experimental_liveOptions({
+      enabled: false,
+    }),
+  );
 
 export type Outputs = InferRouterOutputs<AppRouter>;
 export type Inputs = InferRouterInputs<AppRouter>;
 export type UnwrapAsyncIterator<T> =
-  T extends AsyncIteratorClass<infer TValue, any, any>
-    ? TValue
-    : never;
+  T extends AsyncIteratorClass<infer TValue, any, any> ? TValue : never;
