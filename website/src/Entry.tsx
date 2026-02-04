cbc30ce43a2ec0853deff0229bc04198a7017e2e
Banks Nussman <banks@nussman.us>
2026-02-03 22:51:56 -0500

    chore: identify sentry user on website

--- website/src/Entry.tsx
+++ website/src/Entry.tsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as Sentry from '@sentry/react';
 import { Stack, Box, CircularProgress, Container } from "@mui/material";
 import { useTRPC } from "./utils/trpc";
 import { Header } from "./components/Header";
 import { Banners } from "./components/Banners";
 import { Outlet } from "@tanstack/react-router";
-
 import { useQuery } from "@tanstack/react-query";
 import { useSubscription } from "@trpc/tanstack-react-query";
 import { useQueryClient } from "@tanstack/react-query";
 
 export function Entry() {
   const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
   const { data: user, isLoading } = useQuery(
     trpc.user.me.queryOptions(undefined, {
       retry: false,
       refetchOnWindowFocus: false,
     }),
   );
-  const queryClient = useQueryClient();
 
   useSubscription(
     trpc.user.updates.subscriptionOptions(undefined, {
@@ -27,6 +28,12 @@ export function Entry() {
       },
     }),
   );
+
+  useEffect(() => {
+    if (user) {
+      Sentry.setUser(user);
+    }
+  }, [user]);
 
   if (isLoading) {
     return (
