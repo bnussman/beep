2d78c331f1afa8ce1b9d50aed8e62248d6c373f1
Banks Nussman <banks@nussman.us>
2026-02-03 21:53:03 -0500

    fix: allow sentry tracing header

--- api/src/utils/cors.ts
+++ api/src/utils/cors.ts
@@ -1,5 +1,5 @@
 export const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, Vary",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, Vary, sentry-trace",
 };
