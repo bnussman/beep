8abf1c511f0c54499701d343b3539a3437992cbc
Banks Nussman <banks@nussman.us>
2026-02-08 15:16:21 -0500

    fix: navigation back button

--- app/app/(app)/_layout.tsx
+++ app/app/(app)/_layout.tsx
@@ -4,22 +4,48 @@ import { Stack } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen options={{ headerShown: false }} name="(drawer)" />
-      <Stack.Screen options={{ headerTransparent: true, title: '' }} name="beeps/[id]" />
+    <Stack screenOptions={{
+      headerBackButtonDisplayMode: 'generic'
+    }}>
+      <Stack.Screen
+        options={{ headerShown: false }}
+        name="(drawer)"
+      />
+      <Stack.Screen
+        options={{ headerTransparent: true, title: '' }}
+        name="beeps/[id]"
+      />
       <Stack.Screen
         options={(route) => {
           const params = route.route.params as { id: string };
           return {
-          headerRight: () => {
-            return <UserMenu userId={params.id} />;
-          },
-          unstable_headerRightItems: () => {
-            const options = useUserMenuOptions(params.id);
-            return getNavigationMenuFromOptions(options);
-          },
-        }}}
+            headerRight: () => {
+              return <UserMenu userId={params.id} />;
+            },
+            unstable_headerRightItems: () => {
+              const options = useUserMenuOptions(params.id);
+              return getNavigationMenuFromOptions(options);
+            },
+            headerBackButtonDisplayMode: 'minimal',
+          }
+        }}
         name="user/[id]/index"
+      />
+      <Stack.Screen
+        options={{ headerTitle: "Change Password"}}
+        name="profile/change-password"
+      />
+      <Stack.Screen
+        options={{ headerTitle: "Create Car" }}
+        name="cars/create"
+      />
+      <Stack.Screen
+        options={{ headerTitle: "Report" }}
+        name="user/[id]/report"
+      />
+      <Stack.Screen
+        options={{ headerTitle: "Rate" }}
+        name="user/[id]/rate"
       />
     </Stack>
   );
