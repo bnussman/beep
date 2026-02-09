0044c740c378550ac83d8c8695fb56f8c79d0283
Banks Nussman <banks@nussman.us>
2026-02-08 23:32:12 -0500

    temp: disable api telementry

--- api/src/utils/instrument.ts
+++ api/src/utils/instrument.ts
@@ -6,13 +6,13 @@ Sentry.init({
   environment: ENVIRONMENT,
   debug: false,
   tracesSampler(samplingContext) {
-    return true;
+    return false;
   },
   integrations(integrations) {
     return [
-      Sentry.bunServerIntegration(),
-      Sentry.postgresIntegration(),
-      Sentry.redisIntegration(),
+      // Sentry.bunServerIntegration(),
+      // Sentry.postgresIntegration(),
+      // Sentry.redisIntegration(),
     ];
   },
 });
