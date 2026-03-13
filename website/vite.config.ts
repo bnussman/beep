c3f857e702a98af187ad49e84dfa004f1efc4429
Banks Nussman <banks@nussman.us>
2026-03-12 20:14:52 -0400

    chore(website): update vite to v8

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
