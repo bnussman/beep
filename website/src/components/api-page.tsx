50ef7b11effe445ea53c681ee965a8f97569ea58
Banks Nussman <banks@nussman.us>
2026-04-01 22:03:06 -0400

    fix: ignore error

--- website/src/components/api-page.tsx
+++ website/src/components/api-page.tsx
@@ -1,4 +1,5 @@
 import { openapi } from "../utils/openapi";
 import { createClientAPIPage } from "fumadocs-openapi/ui/create-client";
 
+// @ts-expect-error idk
 export const ClientAPIPage = createClientAPIPage(openapi);
