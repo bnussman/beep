3bb1a07a3bc44242666a9060d36659e887e6d1cf
Banks Nussman <banks@nussman.us>
2026-04-22 19:04:20 -0400

    chore(website): try https://github.com/TanStack/router/discussions/3394#discussioncomment-16512244

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -8,15 +8,44 @@ export default defineConfig({
   },
   plugins: [
     tanstackStart({
-      srcDirectory: "src",
-      router: {
-        routesDirectory: "routes",
-      },
       prerender: {
         enabled: true,
         crawlLinks: false,
       },
-      pages: [{ path: "/" }],
+      // pages: [
+      //   {
+      //     path: "/",
+      //     prerender: {
+      //       outputPath: "/index.html",
+      //       enabled: true,
+      //       crawlLinks: false,
+      //     },
+      //   },
+      //   {
+      //     path: "/tools",
+      //     prerender: {
+      //       outputPath: "/tools.html",
+      //       enabled: true,
+      //       crawlLinks: false,
+      //     },
+      //   },
+      //   {
+      //     path: "/docs",
+      //     prerender: {
+      //       outputPath: "/docs.html",
+      //       enabled: true,
+      //       crawlLinks: true,
+      //     },
+      //   },
+      // ],
+      spa: {
+        enabled: true,
+        prerender: {
+          outputPath: "/index.html",
+          enabled: true,
+          crawlLinks: false,
+        },
+      },
     }),
     react(),
     // babel({ presets: [reactCompilerPreset()] }),
