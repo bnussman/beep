7b8eaa1a508476a40f9eb2c127ae7edc820a7ba3
Banks Nussman <banks@nussman.us>
2026-04-25 17:36:45 -0400

    wip

--- gql/drizzle.config.ts
+++ gql/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { DB_URL } from "./src/utils/constants";
 import { defineConfig } from "drizzle-kit";
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export default defineConfig({
   schema: "drizzle/schema.ts",
