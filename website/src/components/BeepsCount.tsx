d60c53a55bd966f78ab7d0123cdf020296a64074
Banks Nussman <banks@nussman.us>
2026-04-29 00:27:32 -0400

    chore(website): add pulsing icon to landing page

--- website/src/components/BeepsCount.tsx
+++ website/src/components/BeepsCount.tsx
@@ -20,14 +20,39 @@ export function BeepsCount() {
     marginTop: "90px !important",
   };
 
+  const liveBadgeSx = {
+    width: 10,
+    height: 10,
+    mb: 0.8,
+    borderRadius: "50%",
+    backgroundColor: "#34d399",
+    position: "relative",
+    boxShadow: "0 0 0 0 rgba(52,211,153, 0.7)",
+    animation: `pulse 1500ms infinite`,
+    "@keyframes pulse": {
+      "0%": {
+        boxShadow: "0 0 0 0 rgba(52,211,153, 0.7)",
+      },
+      "70%": {
+        boxShadow: "0 0 0 10px rgba(52,211,153, 0)",
+      },
+      "100%": {
+        boxShadow: "0 0 0 0 rgba(52,211,153, 0)",
+      },
+    },
+  } as const;
+
   return (
     <Box sx={sx}>
       <Typography fontSize="2.5rem" minWidth="150px">
         {isLoading ? <Skeleton /> : count.toLocaleString()}
       </Typography>
-      <Typography fontSize="1.125rem" sx={{ mb: 1 }}>
-        beeps
-      </Typography>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 1.25 }}>
+        <Typography fontSize="1.125rem" sx={{ mb: 1 }}>
+          beeps
+        </Typography>
+        <Box sx={liveBadgeSx} />
+      </Box>
     </Box>
   );
 }
