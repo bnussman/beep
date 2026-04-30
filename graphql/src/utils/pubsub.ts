f8a860b4d9f4c1edee77bcd815cb27778b65cf25
Banks Nussman <banks@nussman.us>
2026-04-30 19:55:27 -0400

    rename to graphql

--- graphql/src/utils/pubsub.ts
+++ graphql/src/utils/pubsub.ts
@@ -1,13 +1,13 @@
-import { Context } from "./trpc";
 import { createPubSub } from "@graphql-yoga/subscription";
 import { eventTarget } from "./redis";
-import z from "zod";
-import { user } from "../../drizzle/schema";
+import { beep, user } from "../../drizzle/schema";
 
 export type User = typeof user.$inferSelect;
 
-type Ride = z.infer<typeof rideResponseSchema> | null;
-type Queue = z.infer<typeof queueResponseSchema>;
+type Beep = typeof beep.$inferSelect;
+
+type Ride = Beep | null;
+type Queue = Beep[];
 
 type PubSubChannels = {
   user: [userId: string, payload: { user: User }];
