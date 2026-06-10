14c4c5c9aa305097f7bbcc259b758c963c316eb7
Banks Nussman <banks@nussman.us>
2026-06-10 00:44:19 -0400

    file upload working on iOS and web

--- app/src/utils/files.ts
+++ app/src/utils/files.ts
@@ -1,32 +1,12 @@
 import { isMobile } from "./constants";
-import { File } from "expo-file-system/next";
 import * as ImagePicker from "expo-image-picker";
-
-export class ReactNativeFile {
-  uri: string;
-  name: string;
-  type: string;
-
-  constructor({
-    uri,
-    name,
-    type,
-  }: {
-    uri: string;
-    name: string;
-    type: string;
-  }) {
-    this.uri = uri;
-    this.name = name;
-    this.type = type;
-  }
-}
 
 export async function getFile(
   asset: ImagePicker.ImagePickerAsset,
-): Promise<Blob | ReactNativeFile> {
+): Promise<Blob | File> {
   if (isMobile) {
-    return new File(asset.uri);
+    const { File } = await import("expo-file-system");
+    return new File(asset.uri) as unknown as File;
   }
   const res = await fetch(asset.uri);
   const blob = await res.blob();
