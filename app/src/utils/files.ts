08c53b0166f95ac2b8419e4d13dd36ecdc6425bd
Banks Nussman <banks@nussman.us>
2026-06-10 00:49:17 -0400

    greatly simplify web file logic

--- app/src/utils/files.ts
+++ app/src/utils/files.ts
@@ -3,14 +3,10 @@ import * as ImagePicker from "expo-image-picker";
 
 export async function getFile(
   asset: ImagePicker.ImagePickerAsset,
-): Promise<Blob | File> {
+): Promise<File> {
   if (isMobile) {
     const { File } = await import("expo-file-system");
     return new File(asset.uri) as unknown as File;
   }
-  const res = await fetch(asset.uri);
-  const blob = await res.blob();
-  const fileType = blob.type.split("/")[1];
-  const file = new File([blob], "photo." + fileType);
-  return file;
+  return asset.file!;
 }
