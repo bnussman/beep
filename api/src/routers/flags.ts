5e70b2a9915bc0f155140f6d49ffb468874f6857
Banks Nussman <banks@nussman.us>
2026-07-11 16:10:10 -0400

    chore(api): disable live activities feature flag

--- api/src/routers/flags.ts
+++ api/src/routers/flags.ts
@@ -3,7 +3,7 @@ import { publicProcedure, router } from "../utils/trpc";
 export const flagsRouter = router({
   flags: publicProcedure.query(() => {
     return {
-      liveActivities: true,
+      liveActivities: false,
     };
   }),
 });
