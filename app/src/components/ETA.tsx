446762496b5a6dbe400cb68364e2545b68331796
Banks Nussman <banks@nussman.us>
2026-07-12 18:25:29 -0400

    add some basic formatting to new ETA in rider's view

--- app/src/components/ETA.tsx
+++ app/src/components/ETA.tsx
@@ -6,10 +6,37 @@ interface Props {
 }
 
 export function ETA(props: Props) {
+
+  const renderBody = () => {
+    if (props.eta) {
+      return `Pick up ${getRelativeTimeInMinutes(props.eta)} (at ${new Date(props.eta).toLocaleTimeString([], { timeStyle: 'short' })})`;
+    }
+
+    return "N/A";
+  };
+
   return (
     <View>
       <Text weight="800">ETA</Text>
-      <Text>{props.eta ?? "N/A"}</Text>
+      <Text>{renderBody()}</Text>
     </View>
   );
+}
+
+function getRelativeTimeInMinutes(isoString: string) {
+  const targetDate = new Date(isoString);
+  const now = new Date();
+
+  // Calculate the difference in milliseconds
+  const diffInMs = targetDate.getTime() - now.getTime();
+
+  // Convert milliseconds to total minutes
+  const diffInMinutes = Math.round(diffInMs / 1000 / 60);
+
+  // Return future or past string format
+  if (diffInMinutes >= 0) {
+    return `in ${diffInMinutes} min`;
+  } else {
+    return `${Math.abs(diffInMinutes)} min ago`;
+  }
 }
