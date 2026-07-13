b4e54f3b29bb06202ae143d5f89630056771b452
Banks Nussman <banks@nussman.us>
2026-07-12 21:16:35 -0400

    fix(app): file upload on iOS

--- app/src/utils/files.ts
+++ app/src/utils/files.ts
@@ -1,12 +1,12 @@
 import { isMobile } from "./constants";
 import * as ImagePicker from "expo-image-picker";
+import { File } from 'expo-file-system';
 
 export async function getFile(
   asset: ImagePicker.ImagePickerAsset,
 ): Promise<File> {
   if (isMobile) {
-    const { File } = await import("expo-file-system");
-    return new File(asset.uri) as unknown as File;
+    return new File(asset.uri);
   }
-  return asset.file!;
+  return asset.file! as unknown as File;
 }
