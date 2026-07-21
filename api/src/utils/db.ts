4eabe9f71172d063c3ea57a2b0887fa89bd7c8bb
Banks Nussman <banks@nussman.us>
2026-07-21 00:09:31 -0400

    new db connection for async commits

--- api/src/utils/db.ts
+++ api/src/utils/db.ts
@@ -1,5 +1,5 @@
-import type { ClientConfig } from "pg";
-const { Client } = require("pg"); // we must use `require` because of Bun / Sentry / OpenTelementry weirdness
+import type { ClientConfig, Client as ClientType } from "pg";
+const { Client } = require("pg") as { Client: typeof ClientType }; // we must use `require` because of Bun / Sentry / OpenTelementry weirdness
 import { drizzle } from "drizzle-orm/node-postgres";
 import { DB_URL } from "./constants";
 import { relations } from "../../drizzle/relations";
@@ -12,11 +12,20 @@ const options: ClientConfig = {
 };
 
 const queryClient = new Client(options);
+const writeQueryClient = new Client(options);
 
 await queryClient.connect();
+await writeQueryClient.connect();
+await writeQueryClient.query("SET synchronous_commit = off;");
 
 export const db = drizzle({
   client: queryClient,
+  logger: false,
+  relations,
+});
+
+export const writeDB = drizzle({
+  client: writeQueryClient,
   logger: false,
   relations,
 });
