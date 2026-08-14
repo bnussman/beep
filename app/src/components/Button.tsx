7c64832a8b64f91b4981803cb9543c97d828c2ce
Banks Nussman <banks@nussman.us>
2026-08-14 00:17:52 -0400

    fix button color

--- app/src/components/Button.tsx
+++ app/src/components/Button.tsx
@@ -17,10 +17,12 @@ interface Props {
 export function Button(props: Props & ButtonRootProps) {
   const { children, isLoading, ...rest } = props;
 
+  const themeColorAccentForeground = useThemeColor("accent-foreground");
+
   return (
     <HeroButton {...rest} variant={rest.variant === 'ghost' && isWeb ? 'secondary' : rest.variant}>
       {isLoading ? (
-        <ActivityIndicator />
+        <ActivityIndicator color={themeColorAccentForeground} />
       ) : (
         children
       )}
