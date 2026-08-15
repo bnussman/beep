c064705ac623d4d3a2503d4e8d31531798fb0311
Banks Nussman <banks@nussman.us>
2026-08-15 18:14:44 -0400

    chore(orpc): debug cors

--- orpc/src/index.ts
+++ orpc/src/index.ts
@@ -97,8 +97,13 @@ Bun.serve({
     })
 
     if (response) {
+      response.headers.set("Access-Control-Allow-Origin", "*");
+      response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+      response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization, Vary, sentry-trace, baggage");
+
       return response;
     }
+
 
     return new Response('Not found', { status: 404 })
   },
