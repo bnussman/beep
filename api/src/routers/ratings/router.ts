ea3a90b4506d61f49ac4f77f45d280978d0bddfb
Banks Nussman <banks@nussman.us>
2026-09-12 01:46:29 -0400

    fix(api): typecheck

--- api/src/routers/ratings/router.ts
+++ api/src/routers/ratings/router.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { adminProcedure, authedProcedure } from "../../utils/orpc";
+import { authedProcedure } from "../../utils/orpc";
 import { db } from "../../utils/db";
 import { count, eq } from "drizzle-orm";
 import { ratings, users } from "../../../drizzle/schema";
