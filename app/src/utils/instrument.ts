ed34fe8bfda88c005b78c095fcbffe103bccdf29
Banks Nussman <banks@nussman.us>
2026-09-23 18:00:43 -0400

    build(deps): update sentry in app

--- app/src/utils/instrument.ts
+++ app/src/utils/instrument.ts
@@ -2,10 +2,6 @@ import * as Sentry from "@sentry/react-native";
 import config from "../../package.json";
 import { isWeb } from "./constants";
 
-export const navigationIntegration = Sentry.reactNavigationIntegration({
-  enableTimeToInitialDisplay: true,
-});
-
 Sentry.init({
   release: config.version,
   dsn: "https://22da81efd1744791aa86cfd4bf8ea5eb@o1155818.ingest.sentry.io/6358990",
@@ -14,7 +10,9 @@ Sentry.init({
   enableUserInteractionTracing: true,
   sendDefaultPii: true,
   tracesSampleRate: 0,
-  integrations: [navigationIntegration],
+  integrations: [
+    Sentry.expoRouterIntegration(),
+  ],
   tracePropagationTargets: isWeb
     ? [
       "localhost",
